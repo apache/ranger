043d3da121f968039b3eb87222f83a6876d0f241
Dilli Dorai Arumugam <darumugam@hortonworks.com>
2014-09-11 13:18:25 -0700

    ARGUS-41: Replace XASecure copyright with Apache License Header in source code

--- lookup-client/src/main/java/org/apache/hadoop/security/SecureClientLogin.java
+++ lookup-client/src/main/java/org/apache/hadoop/security/SecureClientLogin.java
@@ -1,14 +1,29 @@
-/**************************************************************************
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+ /**************************************************************************
  *                                                                        *
  * The information in this document is proprietary to XASecure Inc.,      *
  * It may not be used, reproduced or disclosed without the written        *
  * approval from the XASecure Inc.,                                       *
  *                                                                        *
  * PRIVILEGED AND CONFIDENTIAL XASECURE PROPRIETARY INFORMATION           *
- *                                                                        *
- * Copyright (c) 2013 XASecure, Inc.  All rights reserved.                *
- *                                                                        *
- *************************************************************************/
 
 /**
  *
