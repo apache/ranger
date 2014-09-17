6d3618fcbc5fa1ff6f67388ce0bb1c2b9fda47e3
rmani <rmani@hortonworks.com>
2014-09-17 14:47:49 -0700

    Argus-64

--- agents-impl/src/main/java/com/xasecure/pdp/config/PolicyRefresher.java
+++ agents-impl/src/main/java/com/xasecure/pdp/config/PolicyRefresher.java
@@ -1,14 +1,29 @@
-/**************************************************************************
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+ /**************************************************************************
  *                                                                        *
  * The information in this document is proprietary to XASecure Inc.,      *
  * It may not be used, reproduced or disclosed without the written        *
  * approval from the XASecure Inc.,                                       *
  *                                                                        *
  * PRIVILEGED AND CONFIDENTIAL XASECURE PROPRIETARY INFORMATION           *
- *                                                                        *
- * Copyright (c) 2013 XASecure, Inc.  All rights reserved.                *
- *                                                                        *
- *************************************************************************/
 
  /**
   *
@@ -155,7 +170,7 @@ public class PolicyRefresher  {
 	
 	
 	private void savePolicyToFile() {
-		if (!watcherDaemon.iscacheModfied()) {
+		if (watcherDaemon != null && !watcherDaemon.iscacheModfied()) {
 			// Do not Save the file if the policy is not modified.
 			return;
 		}
@@ -186,7 +201,7 @@ public class PolicyRefresher  {
 	
 	 private void savePolicyToCacheFile() {
 		 
-		 if (!watcherDaemon.iscacheModfied()) {
+		 if (watcherDaemon != null && !watcherDaemon.iscacheModfied()) {
 			    // Don't Save the file if the policy is not modified.
 				return;
 		  }
@@ -211,7 +226,9 @@ public class PolicyRefresher  {
 		    finally {
 		    	 //make the policy file cache to be 600 permission when it gets created and updated
 		    	 lastSaveFile.setReadable(false,false);
+		    	 lastSaveFile.setWritable(false,false);
 		    	 lastSaveFile.setReadable(true,true);
+		    	 lastSaveFile.setWritable(true,true);
 		    	 if (writer != null) {
 				 writer.close();
 		    	}
