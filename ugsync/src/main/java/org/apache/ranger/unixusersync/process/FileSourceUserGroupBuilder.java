1d0c14908e9a1c4e8ceb5f27dcd7e7392f3f06bc
rmani <rmani@hortonworks.com>
2015-01-05 00:02:56 -0800

    Ranger-201: Addition changes to process user group JSON array file format in the FileSource

--- ugsync/src/main/java/org/apache/ranger/unixusersync/process/FileSourceUserGroupBuilder.java
+++ ugsync/src/main/java/org/apache/ranger/unixusersync/process/FileSourceUserGroupBuilder.java
@@ -22,39 +22,43 @@ package org.apache.ranger.unixusersync.process;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.apache.ranger.unixusersync.config.UserGroupSyncConfig;
-import org.apache.ranger.unixusersync.model.UserGroupList;
 import org.apache.ranger.usergroupsync.UserGroupSink;
 import org.apache.ranger.usergroupsync.UserGroupSource;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
 
 public class FileSourceUserGroupBuilder  implements UserGroupSource {
-
 	private static final Logger LOG = Logger.getLogger(FileSourceUserGroupBuilder.class) ;
-	
-	public static  String DEFAULT_USER_GROUP_FILE = null;
-	public static String USER_GROUP_FILE = null;
-	
-	private static boolean isManualRun=false;
-	private Map<String,List<String>>  user2GroupListMap = new HashMap<String,List<String>>();
-	private UserGroupSyncConfig config = UserGroupSyncConfig.getInstance() ;
-	private UserGroupSink ugSink     = null ;
-	private long  usergroupFileModified = 0 ;
-	
+
+	private Map<String,List<String>> user2GroupListMap     = new HashMap<String,List<String>>();
+	private UserGroupSyncConfig      config                = UserGroupSyncConfig.getInstance();
+	private String                   userGroupFilename     = null;
+	private long                     usergroupFileModified = 0 ;
+
+
 	public static void main(String[] args) throws Throwable {
+		FileSourceUserGroupBuilder filesourceUGBuilder = new FileSourceUserGroupBuilder() ;
+
 		if (args.length > 0) {
-			isManualRun=true;
-			USER_GROUP_FILE = args[0];
-		}	
-		FileSourceUserGroupBuilder  filesourceUGBuilder = new FileSourceUserGroupBuilder() ;
+			filesourceUGBuilder.setUserGroupFilename(args[0]);
+		}
+		
 		filesourceUGBuilder.init();
+
+		UserGroupSink ugSink = UserGroupSyncConfig.getInstance().getUserGroupSink();
+		LOG.info("initializing sink: " + ugSink.getClass().getName());
+		ugSink.init();
+
+		filesourceUGBuilder.updateSink(ugSink);
+		
 		if ( LOG.isDebugEnabled()) {
 			filesourceUGBuilder.print(); 
 		}
@@ -62,14 +66,36 @@ public class FileSourceUserGroupBuilder  implements UserGroupSource {
 	
 	@Override
 	public void init() throws Throwable {
-		DEFAULT_USER_GROUP_FILE = config.getUserSyncFileSource();
-		buildUserGroupInfo();
-		if (isManualRun) {
-			ugSink = UserGroupSyncConfig.getInstance().getUserGroupSink();
-			LOG.info("initializing sink: " + ugSink.getClass().getName());
-			ugSink.init();
-			updateSink(ugSink);
+		if(userGroupFilename == null) {
+			userGroupFilename = config.getUserSyncFileSource();
 		}
+
+		buildUserGroupInfo();
+	}
+	
+	@Override
+	public boolean isChanged() {
+		long TempUserGroupFileModifedAt = new File(userGroupFilename).lastModified() ;
+		if (usergroupFileModified != TempUserGroupFileModifedAt) {
+			return true ;
+		}
+		return false;
+	}
+
+	@Override
+	public void updateSink(UserGroupSink sink) throws Throwable {
+		buildUserGroupInfo();
+
+		for (Map.Entry<String, List<String>> entry : user2GroupListMap.entrySet()) {
+		    String       user   = entry.getKey();
+		    List<String> groups = entry.getValue();
+
+		    sink.addOrUpdateUser(user, groups);
+		}
+	}
+
+	private void setUserGroupFilename(String filename) {
+		userGroupFilename = filename;
 	}
 
 	private void print() {
@@ -83,28 +109,8 @@ public class FileSourceUserGroupBuilder  implements UserGroupSource {
 			}
 		}
 	}
-	
-	@Override
-	public boolean isChanged() {
-		long TempUserGroupFileModifedAt = new File(DEFAULT_USER_GROUP_FILE).lastModified() ;
-		if (usergroupFileModified != TempUserGroupFileModifedAt) {
-			return true ;
-		}
-		return false;
-	}
-
-	@Override
-	public void updateSink(UserGroupSink sink) throws Throwable {
-		buildUserGroupInfo() ;
-		for (Map.Entry<String, List<String>> entry : user2GroupListMap.entrySet()) {
-		    String       user   = entry.getKey();
-		    List<String> groups = entry.getValue();
-		    sink.addOrUpdateUser(user, groups);
-		}
-	}
 
 	public void buildUserGroupInfo() throws Throwable {
-		user2GroupListMap = new HashMap<String,List<String>>();
 		buildUserGroupList();
 		if ( LOG.isDebugEnabled()) {
 			print(); 
@@ -112,50 +118,90 @@ public class FileSourceUserGroupBuilder  implements UserGroupSource {
 	}
 	
 	public void buildUserGroupList() throws Throwable {
-	
-		Gson gson = new GsonBuilder().create() ;
-		
-		UserGroupList usergrouplist = new UserGroupList();
-		
-		String usergroupFile = null;
-		
-		if (isManualRun) {
-			usergroupFile = USER_GROUP_FILE;
-		} else {
-			usergroupFile = DEFAULT_USER_GROUP_FILE;
-		}
-		
-		if (usergroupFile == null){
+		if (userGroupFilename == null){
 			throw new Exception("User Group Source File is not Configured. Please maintain in unixauthservice.properties or pass it as command line argument for org.apache.ranger.unixusersync.process.FileSourceUserGroupBuilder");
 		}
+	
+		File f = new File(userGroupFilename);
 		
-		File f = new File(usergroupFile);
-		
-		if (f.exists()) {
+		if (f.exists() && f.canRead()) {
 			
-			BufferedReader bfr  = new BufferedReader(new FileReader(f));
+			Map<String,List<String>> tmpUser2GroupListMap = new HashMap<String,List<String>>();
 			
-			String line = null ;
+			JsonReader jsonReader = new JsonReader(new BufferedReader(new FileReader(f)));
 			
-			while ((line = bfr.readLine()) != null) {
+			jsonReader.setLenient(true);
 			
-				if (line.trim().isEmpty()) 
-					continue ;
+			jsonReader.beginArray();
+			
+			while (jsonReader.hasNext() ) {
+				Map<String, List<String>> usergroupMap = getUserGroupMap(jsonReader);
 				
-				usergrouplist = gson.fromJson(line,UserGroupList.class);
-				
-				String user = usergrouplist.getUser();
-				
-				List<String> groups = usergrouplist.getGroups();
-				
-				if ( user != null) {
-					user2GroupListMap.put(user, groups);
-				}
+				for(String user : usergroupMap.keySet()) {
+					List<String> groups = usergroupMap.get(user) ;
+					tmpUser2GroupListMap.put(user,groups);
+				}		
 			}
 			
-			bfr.close();
-			usergroupFileModified = f.lastModified() ;
-		}
+			jsonReader.endArray();
+			
+			jsonReader.close();
 
+			user2GroupListMap     = tmpUser2GroupListMap;
+			
+			usergroupFileModified = f.lastModified() ;
+			
+		} else {
+			throw new Exception("User Group Source File " + userGroupFilename + "doesn't not exist or readable");
+		}
+	}
+	
+	
+	public Map<String, List<String>> getUserGroupMap(JsonReader jsonReader) throws Exception {
+		
+		Map<String, List<String>> ret = new HashMap<String, List<String>>();
+		String user = null ;
+		List<String> groups = new ArrayList<String>();
+		
+		jsonReader.beginObject();
+		
+		while ( jsonReader.hasNext()) {
+			
+			String name = jsonReader.nextName();
+			
+			if ( name.equals("user")) {
+				user = jsonReader.nextString();
+			} else if ( name.equals("groups")) {
+				groups = getGroups(jsonReader);
+			} else {
+				StringBuilder sb = new StringBuilder();
+				sb.append("User Group Source JSON array should have following **user** and **groups** as name tag e.g");
+				sb.append("[ {\"user\":\"userid1\",\"groups\":[\"groupid1\",\"groupid2\"]},");
+				sb.append("[ {\"user\":\"userid2\",\"groups\":[\"groupid1\",\"groupid2\"]}..]");
+				throw new Exception(sb.toString());
+			}
+			
+			if ( user != null ) {
+				ret.put(user, groups);
+			}
+		}
+		
+		jsonReader.endObject();
+		
+		return ret;
+	}
+	
+	public List<String> getGroups(JsonReader reader) throws IOException {
+		List<String> ret = new ArrayList<String>();
+		
+		reader.beginArray();
+		
+		while(reader.hasNext()) {
+			ret.add(reader.nextString());
+		}
+		
+		reader.endArray();
+		
+		return ret;
 	}
 }
