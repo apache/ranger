aa7edc1e99d19934b9918487db8c399c23692459
Alok Lal <alal@hortonworks.com>
2015-02-11 18:45:00 -0800

    RANGER-232 Knox plugin: Removed stale files, moved files out of impl/pdp into knox project
    Signed-off-by: Madhan Neethiraj <madhan@apache.org>

--- knox-agent/src/main/java/org/apache/ranger/authorization/knox/deploy/RangerPDPKnoxDeploymentContributor.java
+++ knox-agent/src/main/java/org/apache/ranger/authorization/knox/deploy/RangerPDPKnoxDeploymentContributor.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.ranger.pdp.knox.deploy;
+package org.apache.ranger.authorization.knox.deploy;
 
 import java.util.ArrayList;
 import java.util.List;
