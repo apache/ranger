88dead26deb617c211d3b0faaa34de2ae46240ac
rmani <rmani@hortonworks.com>
2015-02-20 17:48:05 -0800

    Revert "BUG-32018 : Issue in Hive policy creation in ranger for UDF resource if we specify {{*}} ( access for all UDF)"
    This reverts commit f79476bde51cb9679c442cf80dcbe8b5ccb48c7b.

--- security-admin/src/main/java/com/xasecure/biz/AssetMgr.java
+++ security-admin/src/main/java/com/xasecure/biz/AssetMgr.java
@@ -284,9 +284,6 @@ public class AssetMgr extends AssetMgrBase {
 			}
 		}
 		
-		// set the resourceType based on the resource as there is issue with resourceType coming from UI 
-		// when resource is UDF
-		vXResource.setResourceType(xPolicyService.getResourceType(vXResource));
 		
 		vXResource = xResourceService.createResource(vXResource);
 		
@@ -311,7 +308,7 @@ public class AssetMgr extends AssetMgrBase {
 
 	@Override
 	public VXResource updateXResource(VXResource vXResource) {
-	
+
 		UserSessionBase currentUserSession = ContextUtil
 				.getCurrentUserSession();
 		if (currentUserSession == null) {
@@ -456,12 +453,7 @@ public class AssetMgr extends AssetMgrBase {
 		List<XXTrxLog> trxLogList = xResourceService.getTransactionLog(
 				vXResource, xResouce, "update");
 
-		// set the resourceType based on the resource as there is issue with resourceType coming from UI 
-		// when resource is UDF
-		vXResource.setResourceType(xPolicyService.getResourceType(vXResource));
-		
 		VXResource resource = super.updateXResource(vXResource);
-		
 
 		List<VXPermMap> newPermMapList = vXResource.getPermMapList();
 		searchCriteria = new SearchCriteria();
