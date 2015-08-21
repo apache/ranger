a451e028e14711fb2fe83103d8eab6f8a6d47f1f
Alok Lal <alok@apache.org>
2015-08-20 18:04:35 -0700

    RANGER-622 Hive plugin - add jar via beeline throws NPE in ranger code

--- hive-agent/src/main/java/org/apache/ranger/authorization/hive/authorizer/RangerHiveAuthorizer.java
+++ hive-agent/src/main/java/org/apache/ranger/authorization/hive/authorizer/RangerHiveAuthorizer.java
@@ -241,6 +241,10 @@ public class RangerHiveAuthorizer extends RangerHiveAuthorizerBase {
 				for(HivePrivilegeObject hiveObj : inputHObjs) {
 					RangerHiveResource resource = getHiveResource(hiveOpType, hiveObj);
 
+					if (resource == null) { // possible if input object/object is of a kind that we don't currently authorize
+						continue;
+					}
+
 					if(resource.getObjectType() == HiveObjectType.URI) {
 						String   path       = hiveObj.getObjectName();
 						FsAction permission = FsAction.READ;
@@ -269,6 +273,10 @@ public class RangerHiveAuthorizer extends RangerHiveAuthorizerBase {
 			if(outputHObjs != null) {
 				for(HivePrivilegeObject hiveObj : outputHObjs) {
 					RangerHiveResource resource = getHiveResource(hiveOpType, hiveObj);
+
+					if (resource == null) { // possible if input object/object is of a kind that we don't currently authorize
+						continue;
+					}
 
 					if(resource.getObjectType() == HiveObjectType.URI) {
 						String   path       = hiveObj.getObjectName();
