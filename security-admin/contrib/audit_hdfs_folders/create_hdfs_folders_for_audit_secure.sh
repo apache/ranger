9cb055fc1a2f568f5b5fa9ab8ca0db43c56cf096
Don Bosco Durai <bosco@apache.org>
2015-10-05 21:44:03 -0700

    RANGER-173: Applied review feedback to use variables for user/group

--- security-admin/contrib/audit_hdfs_folders/create_hdfs_folders_for_audit_secure.sh
+++ security-admin/contrib/audit_hdfs_folders/create_hdfs_folders_for_audit_secure.sh
@@ -14,54 +14,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#Usage: Run this script as user hdfs. 
-#Creating folders required for Apache Ranger auditing to HDFS. 
-#Note 1: Use this script only for non-secure/kerberos environment
-#
+#Usage: Run this script as user hdfs or the HDFS admin user. 
+#This script creates the folders in HDFS required by Apache Ranger for writing Audit records
+#Note 1: Use this script only for non-kerberos environment. In non-kerberos environment, Ranger KMS writes the audit logs as user "HTTP"
+#Note 2: Please update the below variables according to your environment
+
+HBASE_USER_GROUP=hbase:hbase
+HDFS_USER_GROUP=hdfs:hdfs
+HIVE_USER_GROUP=hive:hive
+KAFKA_USER_GROUP=kafka:kafka
+KMS_USER_GROUP=HTTP:HTTP
+KNOX_USER_GROUP=knox:knox
+SOLR_USER_GROUP=solr:solr
+STORM_USER_GROUP=storm:storm
+YARN_USER_GROUP=yarn:yarn
 
 set -x
+
+#Create parent folder with rx permission
 hdfs dfs -mkdir -p /ranger/audit
-hdfs dfs -chown hdfs:hdfs /ranger/audit
+hdfs dfs -chown $HDFS_USER_GROUP /ranger/audit
 hdfs dfs -chmod 755 /ranger
 hdfs dfs -chmod 755 /ranger/audit
 
-
 hdfs dfs -mkdir -p /ranger/audit/hbaseMaster
-hdfs dfs -chown hbase:hbase /ranger/audit/hbaseMaster
+hdfs dfs -chown $HBASE_USER_GROUP /ranger/audit/hbaseMaster
 hdfs dfs -chmod -R 0700 /ranger/audit/hbaseMaster
 
 hdfs dfs -mkdir -p /ranger/audit/hbaseRegional
-hdfs dfs -chown hbase:hbase /ranger/audit/hbaseRegional
+hdfs dfs -chown $HBASE_USER_GROUP /ranger/audit/hbaseRegional
 hdfs dfs -chmod -R 0700 /ranger/audit/hbaseRegional
 
 hdfs dfs -mkdir -p /ranger/audit/hdfs
-hdfs dfs -chown hdfs:hdfs /ranger/audit/hdfs
+hdfs dfs -chown $HDFS_USER_GROUP /ranger/audit/hdfs
 hdfs dfs -chmod -R 0700 /ranger/audit/hdfs
 
 hdfs dfs -mkdir -p /ranger/audit/hiveServer2
-hdfs dfs -chown hive:hive /ranger/audit/hiveServer2
+hdfs dfs -chown $HIVE_USER_GROUP /ranger/audit/hiveServer2
 hdfs dfs -chmod -R 0700 /ranger/audit/hiveServer2
 
 hdfs dfs -mkdir -p /ranger/audit/kafka
-hdfs dfs -chown kafka:kafka /ranger/audit/kafka
+hdfs dfs -chown $KAFKA_USER_GROUP /ranger/audit/kafka
 hdfs dfs -chmod -R 0700 /ranger/audit/kafka
 
 hdfs dfs -mkdir -p /ranger/audit/kms
-hdfs dfs -chown HTTP:HTTP /ranger/audit/kms
+hdfs dfs -chown $KMS_USER_GROUP /ranger/audit/kms
 hdfs dfs -chmod -R 0700 /ranger/audit/kms
 
 hdfs dfs -mkdir -p /ranger/audit/knox
-hdfs dfs -chown knox:knox /ranger/audit/knox
+hdfs dfs -chown $KNOX_USER_GROUP /ranger/audit/knox
 hdfs dfs -chmod -R 0700 /ranger/audit/knox
 
 hdfs dfs -mkdir -p /ranger/audit/solr
-hdfs dfs -chown solr:solr /ranger/audit/solr
+hdfs dfs -chown $SOLR_USER_GROUP /ranger/audit/solr
 hdfs dfs -chmod -R 0700 /ranger/audit/solr
 
 hdfs dfs -mkdir -p /ranger/audit/storm
-hdfs dfs -chown storm:storm /ranger/audit/storm
+hdfs dfs -chown $STORM_USER_GROUP /ranger/audit/storm
 hdfs dfs -chmod -R 0700 /ranger/audit/storm
 
 hdfs dfs -mkdir -p /ranger/audit/yarn
-hdfs dfs -chown yarn:yarn /ranger/audit/yarn
+hdfs dfs -chown $YARN_USER_GROUP /ranger/audit/yarn
 hdfs dfs -chmod -R 0700 /ranger/audit/yarn
+
