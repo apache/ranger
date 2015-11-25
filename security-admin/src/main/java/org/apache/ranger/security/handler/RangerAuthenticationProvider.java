7a95ff0de1ca4b340deebf6c53e8559a025df750
Gautam Borad <gautam@apache.org>
2015-11-25 17:35:17 +0530

    RANGER-743 : External users with Admin Role should be allowed to create/update users

--- security-admin/src/main/java/org/apache/ranger/security/handler/RangerAuthenticationProvider.java
+++ security-admin/src/main/java/org/apache/ranger/security/handler/RangerAuthenticationProvider.java
@@ -20,6 +20,7 @@
 package org.apache.ranger.security.handler;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -241,6 +242,7 @@ public class RangerAuthenticationProvider implements AuthenticationProvider {
 
 				authentication = ldapAuthenticationProvider
 						.authenticate(finalAuthentication);
+				authentication=getAuthenticationWithGrantedAuthority(authentication);
 				return authentication;
 			} else {
 				return authentication;
@@ -334,6 +336,7 @@ public class RangerAuthenticationProvider implements AuthenticationProvider {
 						principal, userPassword, grantedAuths);
 				authentication = jaasAuthenticationProvider
 						.authenticate(finalAuthentication);
+				authentication=getAuthenticationWithGrantedAuthority(authentication);
 				return authentication;
 			} else {
 				return authentication;
@@ -410,6 +413,7 @@ public class RangerAuthenticationProvider implements AuthenticationProvider {
 				final Authentication finalAuthentication = new UsernamePasswordAuthenticationToken(principal, userPassword, grantedAuths);
 
 				authentication = ldapAuthenticationProvider.authenticate(finalAuthentication);
+				authentication=getAuthenticationWithGrantedAuthority(authentication);
 				return authentication;
 			} else {
 				return authentication;
@@ -475,6 +479,7 @@ public class RangerAuthenticationProvider implements AuthenticationProvider {
 				final Authentication finalAuthentication = new UsernamePasswordAuthenticationToken(principal, userPassword, grantedAuths);
 
 				authentication = ldapAuthenticationProvider.authenticate(finalAuthentication);
+				authentication=getAuthenticationWithGrantedAuthority(authentication);
 				return authentication;
 			} else {
 				return authentication;
@@ -510,8 +515,6 @@ public class RangerAuthenticationProvider implements AuthenticationProvider {
 			if (userName != null && userPassword != null && !userName.trim().isEmpty()&& !userPassword.trim().isEmpty()) {
 				final List<GrantedAuthority> grantedAuths = new ArrayList<>();
 				grantedAuths.add(new SimpleGrantedAuthority(rangerLdapDefaultRole));
-				grantedAuths.add(new SimpleGrantedAuthority("ROLE_SYS_ADMIN"));
-				grantedAuths.add(new SimpleGrantedAuthority("ROLE_KEY_ADMIN"));
 				final UserDetails principal = new User(userName, userPassword,grantedAuths);
 				final Authentication finalAuthentication = new UsernamePasswordAuthenticationToken(principal, userPassword, grantedAuths);
 				authentication= authenticator.authenticate(finalAuthentication);
@@ -529,6 +532,27 @@ public class RangerAuthenticationProvider implements AuthenticationProvider {
 			throw e;
 		}catch (Exception e) {
 			throw e;
+		}
+		return authentication;
+	}
+	
+	private List<GrantedAuthority> getAuthorities(String username) {
+		Collection<String> roleList=userMgr.getRolesByLoginId(username);
+		final List<GrantedAuthority> grantedAuths = new ArrayList<>();
+		for(String role:roleList){
+			grantedAuths.add(new SimpleGrantedAuthority(role));
+		}
+		return grantedAuths;
+	}
+ 
+	public Authentication getAuthenticationWithGrantedAuthority(Authentication authentication){
+		UsernamePasswordAuthenticationToken result=null;
+		if(authentication!=null && authentication.isAuthenticated()){
+			final List<GrantedAuthority> grantedAuths=getAuthorities(authentication.getName().toString());
+			final UserDetails userDetails = new User(authentication.getName().toString(), authentication.getCredentials().toString(),grantedAuths);
+			result = new UsernamePasswordAuthenticationToken(userDetails,authentication.getCredentials(),grantedAuths);
+			result.setDetails(authentication.getDetails());
+			return result;
 		}
 		return authentication;
 	}
