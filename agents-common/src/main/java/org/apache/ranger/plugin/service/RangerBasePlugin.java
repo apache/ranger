0ecac71fd5fc61642fef9b25fb2e12ea69052187
Alok Lal <alok@apache.org>
2016-03-17 12:19:42 -0700

    RANGER-882 Add a catch all policy engine initialization to ensure that plugin will continue to pull policies periodically

--- agents-common/src/main/java/org/apache/ranger/plugin/service/RangerBasePlugin.java
+++ agents-common/src/main/java/org/apache/ranger/plugin/service/RangerBasePlugin.java
@@ -106,14 +106,20 @@ public class RangerBasePlugin {
 	}
 
 	public void setPolicies(ServicePolicies policies) {
-		RangerPolicyEngine oldPolicyEngine = this.policyEngine;
 
-		RangerPolicyEngine policyEngine = new RangerPolicyEngineImpl(appId, policies, policyEngineOptions);
+		// guard against catastrophic failure during policy engine Initialization or
+		try {
+			RangerPolicyEngine oldPolicyEngine = this.policyEngine;
 
-		this.policyEngine = policyEngine;
+			RangerPolicyEngine policyEngine = new RangerPolicyEngineImpl(appId, policies, policyEngineOptions);
 
-		if (oldPolicyEngine != null && !oldPolicyEngine.preCleanup()) {
-			LOG.error("preCleanup() failed on the previous policy engine instance !!");
+			this.policyEngine = policyEngine;
+
+			if (oldPolicyEngine != null && !oldPolicyEngine.preCleanup()) {
+				LOG.error("preCleanup() failed on the previous policy engine instance !!");
+			}
+		} catch (Exception e) {
+			LOG.error("setPolicies: policy engine initialization failed!  Leaving current policy engine as-is.");
 		}
 	}
 
