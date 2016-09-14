ed3f214b2bb44278b27826e7e5de5990df4f62eb
Colm O hEigeartaigh <coheigea@apache.org>
2016-09-14 15:20:05 +0100

    Adding row masking test for Hive

--- hive-agent/src/test/java/org/apache/ranger/services/hive/HIVERangerAuthorizerTest.java
+++ hive-agent/src/test/java/org/apache/ranger/services/hive/HIVERangerAuthorizerTest.java
@@ -46,6 +46,7 @@ import org.junit.Test;
  * a) A user "bob" can do a select/update on the table "words"
  * b) A group called "IT" can do a select only on the "count" column in "words"
  * c) "bob" can create any database
+ * d) "dave" can do a select on the table "words" but only if the "count" column is >= 80
  * 
  */
 public class HIVERangerAuthorizerTest {
@@ -518,5 +519,45 @@ public class HIVERangerAuthorizerTest {
         connection.close();
     }
     
+    @Test
+    public void testHiveRowFilter() throws Exception {
+        
+        // dave can do a select where the count is >= 80
+        String url = "jdbc:hive2://localhost:" + port + "/rangerauthz";
+        Connection connection = DriverManager.getConnection(url, "dave", "dave");
+        Statement statement = connection.createStatement();
+
+        // "dave" can select where count >= 80
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM words where count == '100'");
+        if (resultSet.next()) {
+        	Assert.assertEquals("Mr.", resultSet.getString(1));
+        	Assert.assertEquals(100, resultSet.getInt(2));
+        } else {
+        	Assert.fail("No ResultSet found");
+        }
+        
+        resultSet = statement.executeQuery("SELECT * FROM words where count == '79'");
+        if (resultSet.next()) {
+        	Assert.fail("Authorization should not be granted for count < 80");
+        }
+        
+        statement.close();
+        connection.close();
+        
+        // "bob" should be able to read a count of "79" as the filter doesn't apply to him
+        connection = DriverManager.getConnection(url, "bob", "bob");
+        statement = connection.createStatement();
+        
+        resultSet = statement.executeQuery("SELECT * FROM words where count == '79'");
+        if (resultSet.next()) {
+        	Assert.assertEquals("cannot", resultSet.getString(1));
+        	Assert.assertEquals(79, resultSet.getInt(2));
+        } else {
+        	Assert.fail("No ResultSet found");
+        }
+        
+        statement.close();
+        connection.close();
+    }
     
 }
