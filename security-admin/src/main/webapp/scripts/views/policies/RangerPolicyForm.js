8a728b5d78495524da75a057d2eb4fd9248f2cef
zhangqiang2 <zhangqiang2@zte.com.cn>
2017-09-18 09:17:17 +0800

    RANGER-1782 When the drop-down box is switched, more than one symbol is displayed each time in ranger-web-hivedev
    Signed-off-by: peng.jianhua <peng.jianhua@zte.com.cn>

--- security-admin/src/main/webapp/scripts/views/policies/RangerPolicyForm.js
+++ security-admin/src/main/webapp/scripts/views/policies/RangerPolicyForm.js
@@ -333,7 +333,8 @@ define(function(require){
 						field.editor.validators = this.defaultValidator[key];
 						if($.inArray('required',field.editor.validators) >= 0){
 							var label = field.$el.find('label').html();
-							field.$el.find('label').html(label+"*");
+							label=label.indexOf("*")>-1?label:label+"*";
+							field.$el.find('label').html(label);
 						}
 					}
 				}
