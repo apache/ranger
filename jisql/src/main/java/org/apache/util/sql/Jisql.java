49f874fcf430eff8ec57e1864202f6c1d06eeef2
pradeep <pradeep@apache.org>
2017-10-16 09:28:55 +0530

    RANGER-1838: Refactor Jisql dependencies

--- jisql/src/main/java/org/apache/util/sql/Jisql.java
+++ jisql/src/main/java/org/apache/util/sql/Jisql.java
@@ -62,9 +62,7 @@ import org.apache.util.outputformatter.JisqlFormatter;
  * default is the term "go" on a single line like Sybase's isql or MS/SQL's
  * isql/osql. Note that there is a dependency on <a
  * href="http://jopt-simple.sourceforge.net/">JOpt Simple</a> in for the base
- * configuration. Additionally, if you are using the CSVFormatter then it is
- * dependent on <a href="http://sourceforge.net/projects/javacsv/">Java CSV</a>.
- * <p>
+ * configuration.
  *
  *
  * Options:
