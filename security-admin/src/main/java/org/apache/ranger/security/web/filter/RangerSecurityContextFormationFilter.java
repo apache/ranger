ba99369a82914101d0652d68056a0b48eb69eea1
Nikhil P <nikhil.purbhe@gmail.com>
2017-10-17 16:16:58 +0530

    RANGER-1828 : Good coding practice-add additional headers in ranger
    Signed-off-by: Mehul Parikh <mehul@apache.org>

--- security-admin/src/main/java/org/apache/ranger/security/web/filter/RangerSecurityContextFormationFilter.java
+++ security-admin/src/main/java/org/apache/ranger/security/web/filter/RangerSecurityContextFormationFilter.java
@@ -139,6 +139,9 @@ public class RangerSecurityContextFormationFilter extends GenericFilterBean {
 			}
 			HttpServletResponse res = (HttpServletResponse)response;
 			res.setHeader("X-Frame-Options", "DENY" );
+			res.setHeader("X-Content-Type-Options", "nosniff");
+			res.setHeader("X-XSS-Protection", "1; mode=block");
+			res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
 			chain.doFilter(request, res);
 
 		} finally {
