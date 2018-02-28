9a3d4e306b5e536e294cb20fb67664e2e0b7a7f8
nixonrodrigues <nixon@apache.org>
2018-02-27 18:37:58 -0800

    RANGER-2004: updated Ranger authorization plugin for Atlas for the changes in ATLAS-2459

--- plugin-atlas/src/main/java/org/apache/ranger/authorization/atlas/authorizer/RangerAtlasAuthorizer.java
+++ plugin-atlas/src/main/java/org/apache/ranger/authorization/atlas/authorizer/RangerAtlasAuthorizer.java
@@ -25,14 +25,31 @@ import org.apache.atlas.authorize.AtlasAuthorizationException;
 import org.apache.atlas.authorize.AtlasEntityAccessRequest;
 import org.apache.atlas.authorize.AtlasTypeAccessRequest;
 import org.apache.atlas.authorize.AtlasAuthorizer;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ranger.audit.model.AuthzAuditEvent;
 import org.apache.ranger.plugin.audit.RangerDefaultAuditHandler;
+import org.apache.ranger.plugin.model.RangerServiceDef;
+import org.apache.ranger.plugin.policyengine.RangerAccessRequestImpl;
+import org.apache.ranger.plugin.policyengine.RangerAccessResourceImpl;
+import org.apache.ranger.plugin.policyengine.RangerAccessResult;
 import org.apache.ranger.plugin.service.RangerBasePlugin;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.ranger.plugin.util.RangerPerfTracer;
+
+import static org.apache.ranger.services.atlas.RangerServiceAtlas.RESOURCE_TYPE_CATEGORY;
+import static org.apache.ranger.services.atlas.RangerServiceAtlas.RESOURCE_TYPE_NAME;
+import static org.apache.ranger.services.atlas.RangerServiceAtlas.RESOURCE_ENTITY_TYPE;
+import static org.apache.ranger.services.atlas.RangerServiceAtlas.RESOURCE_ENTITY_CLASSIFICATION;
+import static org.apache.ranger.services.atlas.RangerServiceAtlas.RESOURCE_ENTITY_ID;
+import static org.apache.ranger.services.atlas.RangerServiceAtlas.RESOURCE_SERVICE;
+
+import java.util.*;
 
 
 public class RangerAtlasAuthorizer implements AtlasAuthorizer {
-    private static final Logger LOG = LoggerFactory.getLogger(RangerAtlasAuthorizer.class);
+    private static final Log LOG      = LogFactory.getLog(RangerAtlasAuthorizer.class);
+    private static final Log PERF_LOG = RangerPerfTracer.getPerfLogger("atlasauth.request");
 
     private static volatile RangerBasePlugin atlasPlugin = null;
 
@@ -73,15 +90,33 @@ public class RangerAtlasAuthorizer implements AtlasAuthorizer {
     @Override
     public boolean isAccessAllowed(AtlasAdminAccessRequest request) throws AtlasAuthorizationException {
         if (LOG.isDebugEnabled()) {
-            LOG.debug("==> isAccessAllowed(AtlasAdminAccessRequest)");
+            LOG.debug("==> isAccessAllowed(" + request + ")");
         }
 
-        final boolean ret;
+        final boolean    ret;
+        RangerPerfTracer perf = null;
 
-        ret = true; // TODO: evaluate Ranger policies
+        try {
+            if (RangerPerfTracer.isPerfTraceEnabled(PERF_LOG)) {
+                perf = RangerPerfTracer.getPerfTracer(PERF_LOG, "RangerAtlasAuthorizer.isAccessAllowed(" + request + ")");
+            }
+
+            String                   action         = request.getAction() != null ? request.getAction().getType() : null;
+            RangerAccessResourceImpl rangerResource = new RangerAccessResourceImpl(Collections.singletonMap(RESOURCE_SERVICE, "*"));
+            RangerAccessRequestImpl  rangerRequest  = new RangerAccessRequestImpl(rangerResource, action, request.getUser(), request.getUserGroups());
+
+            rangerRequest.setClientIPAddress(request.getClientIPAddress());
+            rangerRequest.setAccessTime(request.getAccessTime());
+            rangerRequest.setAction(action);
+            rangerRequest.setClusterName(getClusterName());
+
+            ret = checkAccess(rangerRequest);
+        } finally {
+            RangerPerfTracer.log(perf);
+        }
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("<== isAccessAllowed(AtlasAdminAccessRequest)");
+            LOG.debug("<== isAccessAllowed(" + request + "): " + ret);
         }
 
         return ret;
@@ -90,15 +125,82 @@ public class RangerAtlasAuthorizer implements AtlasAuthorizer {
     @Override
     public boolean isAccessAllowed(AtlasEntityAccessRequest request) throws AtlasAuthorizationException {
         if (LOG.isDebugEnabled()) {
-            LOG.debug("==> isAccessAllowed(AtlasEntityAccessRequest)");
+            LOG.debug("==> isAccessAllowed(" + request + ")");
         }
 
-        final boolean ret;
+        boolean          ret  = false;
+        RangerPerfTracer perf = null;
+        RangerAtlasAuditHandler auditHandler = new RangerAtlasAuditHandler(request, getServiceDef());
 
-        ret = true; // TODO: evaluate Ranger policies
+        try {
+            if (RangerPerfTracer.isPerfTraceEnabled(PERF_LOG)) {
+                perf = RangerPerfTracer.getPerfTracer(PERF_LOG, "RangerAtlasAuthorizer.isAccessAllowed(" + request + ")");
+            }
+
+            final String            action         = request.getAction() != null ? request.getAction().getType() : null;
+            final Set<String>       entityTypes    = request.getEntityTypeAndAllSuperTypes();
+            final String            entityId       = request.getEntityId();
+            final String            classification = request.getClassification() != null ? request.getClassification().getTypeName() : null;
+            RangerAccessRequestImpl rangerRequest  = new RangerAccessRequestImpl();
+
+            rangerRequest.setAccessType(action);
+            rangerRequest.setAction(action);
+            rangerRequest.setUser(request.getUser());
+            rangerRequest.setUserGroups(request.getUserGroups());
+            rangerRequest.setClientIPAddress(request.getClientIPAddress());
+            rangerRequest.setAccessTime(request.getAccessTime());
+            rangerRequest.setClusterName(getClusterName());
+
+            final Set<String> classificationsToAuthorize;
+
+            if (classification != null) {
+                if (request.getEntityClassifications() == null) {
+                    classificationsToAuthorize = Collections.singleton(classification);
+                } else {
+                    classificationsToAuthorize = new HashSet<>(request.getEntityClassifications());
+
+                    classificationsToAuthorize.add(classification);
+                }
+            } else {
+                classificationsToAuthorize = request.getEntityClassifications();
+            }
+
+            if (CollectionUtils.isNotEmpty(classificationsToAuthorize)) {
+                // check authorization for each classification
+                for (String classificationToAuthorize : classificationsToAuthorize) {
+                    RangerAccessResourceImpl rangerResource = new RangerAccessResourceImpl();
+
+                    rangerResource.setValue(RESOURCE_ENTITY_TYPE, entityTypes);
+                    rangerResource.setValue(RESOURCE_ENTITY_CLASSIFICATION, request.getClassificationTypeAndAllSuperTypes(classificationToAuthorize));
+                    rangerResource.setValue(RESOURCE_ENTITY_ID, entityId);
+
+                    rangerRequest.setResource(rangerResource);
+
+                    ret = checkAccess(rangerRequest, auditHandler);
+
+                    if (!ret) {
+                        break;
+                    }
+                }
+            } else { // no classifications to authorize
+                RangerAccessResourceImpl rangerResource = new RangerAccessResourceImpl();
+
+                rangerResource.setValue(RESOURCE_ENTITY_TYPE, entityTypes);
+                rangerResource.setValue(RESOURCE_ENTITY_CLASSIFICATION, Collections.<String>emptySet());
+                rangerResource.setValue(RESOURCE_ENTITY_ID, entityId);
+
+                rangerRequest.setResource(rangerResource);
+
+                ret = checkAccess(rangerRequest, auditHandler);
+            }
+        } finally {
+            auditHandler.flushAudit();
+
+            RangerPerfTracer.log(perf);
+        }
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("<== isAccessAllowed(AtlasEntityAccessRequest)");
+            LOG.debug("<== isAccessAllowed(" + request + "): " + ret);
         }
 
         return ret;
@@ -107,15 +209,82 @@ public class RangerAtlasAuthorizer implements AtlasAuthorizer {
     @Override
     public boolean isAccessAllowed(AtlasTypeAccessRequest request) throws AtlasAuthorizationException {
         if (LOG.isDebugEnabled()) {
-            LOG.debug("==> isAccessAllowed(AtlasTypeAccessRequest)");
+            LOG.debug("==> isAccessAllowed(" + request + ")");
         }
 
-        final boolean ret;
+        final boolean    ret;
+        RangerPerfTracer perf = null;
 
-        ret = true; // TODO: evaluate Ranger policies
+        try {
+            if (RangerPerfTracer.isPerfTraceEnabled(PERF_LOG)) {
+                perf = RangerPerfTracer.getPerfTracer(PERF_LOG, "RangerAtlasAuthorizer.isAccessAllowed(" + request + ")");
+            }
+
+            final String typeName     = request.getTypeDef() != null ? request.getTypeDef().getName() : null;
+            final String typeCategory = request.getTypeDef() != null && request.getTypeDef().getCategory() != null ? request.getTypeDef().getCategory().name() : null;
+            final String action       = request.getAction() != null ? request.getAction().getType() : null;
+
+            RangerAccessResourceImpl rangerResource = new RangerAccessResourceImpl();
+
+            rangerResource.setValue(RESOURCE_TYPE_NAME, typeName);
+            rangerResource.setValue(RESOURCE_TYPE_CATEGORY, typeCategory);
+
+            RangerAccessRequestImpl rangerRequest = new RangerAccessRequestImpl(rangerResource, action, request.getUser(), request.getUserGroups());
+            rangerRequest.setClientIPAddress(request.getClientIPAddress());
+            rangerRequest.setAccessTime(request.getAccessTime());
+            rangerRequest.setClusterName(getClusterName());
+            rangerRequest.setAction(action);
+
+
+            ret = checkAccess(rangerRequest);
+        } finally {
+            RangerPerfTracer.log(perf);
+        }
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("<== isAccessAllowed(AtlasTypeAccessRequest)");
+            LOG.debug("<== isAccessAllowed(" + request + "): " + ret);
+        }
+
+        return ret;
+    }
+
+    private String getClusterName() {
+        RangerBasePlugin plugin = atlasPlugin;
+
+        return plugin != null ? plugin.getClusterName() : null;
+    }
+
+    private RangerServiceDef getServiceDef() {
+        RangerBasePlugin plugin = atlasPlugin;
+
+        return plugin != null ? plugin.getServiceDef() : null;
+    }
+
+    private boolean checkAccess(RangerAccessRequestImpl request) {
+        boolean          ret    = false;
+        RangerBasePlugin plugin = atlasPlugin;
+
+        if (plugin != null) {
+            RangerAccessResult result = plugin.isAccessAllowed(request);
+
+            ret = result != null && result.getIsAllowed();
+        } else {
+            LOG.warn("RangerAtlasPlugin not initialized. Access blocked!!!");
+        }
+
+        return ret;
+    }
+
+    private boolean checkAccess(RangerAccessRequestImpl request, RangerAtlasAuditHandler auditHandler) {
+        boolean          ret    = false;
+        RangerBasePlugin plugin = atlasPlugin;
+
+        if (plugin != null) {
+            RangerAccessResult result = plugin.isAccessAllowed(request, auditHandler);
+
+            ret = result != null && result.getIsAllowed();
+        } else {
+            LOG.warn("RangerAtlasPlugin not initialized. Access blocked!!!");
         }
 
         return ret;
@@ -124,6 +293,65 @@ public class RangerAtlasAuthorizer implements AtlasAuthorizer {
     class RangerAtlasPlugin extends RangerBasePlugin {
         RangerAtlasPlugin() {
             super("atlas", "atlas");
+        }
+    }
+
+    class RangerAtlasAuditHandler extends RangerDefaultAuditHandler {
+        private final Map<Long, AuthzAuditEvent> auditEvents;
+        private final String                     resourcePath;
+        private       boolean                    denyExists = false;
+
+
+        public RangerAtlasAuditHandler(AtlasEntityAccessRequest request, RangerServiceDef serviceDef) {
+            Collection<String> classifications    = request.getEntityClassifications();
+            String             strClassifications = classifications == null ? "[]" : classifications.toString();
+
+            if (request.getClassification() != null) {
+                strClassifications += ("," + request.getClassification().getTypeName());
+            }
+
+            RangerAccessResourceImpl rangerResource = new RangerAccessResourceImpl();
+
+            rangerResource.setServiceDef(serviceDef);
+            rangerResource.setValue(RESOURCE_ENTITY_TYPE, request.getEntityType());
+            rangerResource.setValue(RESOURCE_ENTITY_CLASSIFICATION, strClassifications);
+            rangerResource.setValue(RESOURCE_ENTITY_ID, request.getEntityId());
+
+            auditEvents  = new HashMap<>();
+            resourcePath = rangerResource.getAsString();
+        }
+
+        @Override
+        public void processResult(RangerAccessResult result) {
+            if (denyExists) { // nothing more to do, if a deny already encountered
+                return;
+            }
+
+            AuthzAuditEvent auditEvent = super.getAuthzEvents(result);
+
+            if (auditEvent != null) {
+                // audit event might have list of entity-types and classification-types; overwrite with the values in original request
+                if (resourcePath != null) {
+                    auditEvent.setResourcePath(resourcePath);
+                }
+
+                if (!result.getIsAllowed()) {
+                    denyExists = true;
+
+                    auditEvents.clear();
+                }
+
+                auditEvents.put(auditEvent.getPolicyId(), auditEvent);
+            }
+        }
+
+
+        public void flushAudit() {
+            if (auditEvents != null) {
+                for (AuthzAuditEvent auditEvent : auditEvents.values()) {
+                    logAuthzAudit(auditEvent);
+                }
+            }
         }
     }
 }
--- plugin-atlas/src/main/java/org/apache/ranger/services/atlas/RangerServiceAtlas.java
+++ plugin-atlas/src/main/java/org/apache/ranger/services/atlas/RangerServiceAtlas.java
@@ -18,24 +18,61 @@
  */
 package org.apache.ranger.services.atlas;
 
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.Gson;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOCase;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.ranger.plugin.client.BaseClient;
+import org.apache.ranger.plugin.client.HadoopException;
 import org.apache.ranger.plugin.model.RangerPolicy;
 import org.apache.ranger.plugin.model.RangerService;
 import org.apache.ranger.plugin.model.RangerServiceDef;
 import org.apache.ranger.plugin.service.RangerBaseService;
 import org.apache.ranger.plugin.service.ResourceLookupContext;
-import org.apache.ranger.services.atlas.client.AtlasResourceMgr;
+import org.apache.ranger.plugin.util.PasswordUtils;
+
+import javax.security.auth.Subject;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.NewCookie;
 
 public class RangerServiceAtlas extends RangerBaseService {
-
 	private static final Log LOG = LogFactory.getLog(RangerServiceAtlas.class);
+
+	public static final String RESOURCE_SERVICE               = "atlas-service";
+	public static final String RESOURCE_TYPE_CATEGORY         = "type-category";
+	public static final String RESOURCE_TYPE_NAME             = "type";
+	public static final String RESOURCE_ENTITY_TYPE           = "entity-type";
+	public static final String RESOURCE_ENTITY_CLASSIFICATION = "entity-classification";
+	public static final String RESOURCE_ENTITY_ID             = "entity";
+	public static final String CONFIG_REST_ADDRESS            = "atlas.rest.address";
+	public static final String CONFIG_USERNAME                = "username";
+	public static final String CONFIG_PASSWORD                = "password";
+
+	private static final String TYPE_ENTITY         = "entity";
+	private static final String TYPE_CLASSIFICATION = "classification";
+	private static final String TYPE_STRUCT         = "struct";
+	private static final String TYPE_ENUM           = "enum";
+	private static final String TYPE_RELATIONSHIP   = "relationship";
+
+	private static final String URL_LOGIN                = "/j_spring_security_check";
+	private static final String URL_GET_TYPESDEF_HEADERS = "/api/atlas/v2/types/typedefs/headers";
+
+	private static final String WEB_RESOURCE_CONTENT_TYPE = "application/x-www-form-urlencoded";
+	private static final String CONNECTION_ERROR_MSG      =   " You can still save the repository and start creating"
+	                                                        + " policies, but you would not be able to use autocomplete for"
+	                                                        + " resource names. Check ranger_admin.log for more info.";
 
 	public RangerServiceAtlas() {
 		super();
@@ -48,44 +85,35 @@ public class RangerServiceAtlas extends RangerBaseService {
 
 	@Override
 	public Map<String, Object> validateConfig() throws Exception {
-		Map<String, Object> responseMap = new HashMap<String, Object>();
-		String serviceName = getServiceName();
 		if (LOG.isDebugEnabled()) {
-			LOG.debug("==> RangerServiceAtlas.validateConfig Service: ("+ serviceName + " )");
+			LOG.debug("==> RangerServiceAtlas.validateConfig()");
 		}
-		if (configs != null) {
-			try {
-				responseMap = AtlasResourceMgr.validateConfig(serviceName,configs);
-			} catch (Exception e) {
-				LOG.error("<== RangerServiceAtlas.validateConfig Error:" + e);
-				throw e;
-			}
-		}
+
+		AtlasServiceClient client = new AtlasServiceClient(getServiceName(), configs);
+
+		Map<String, Object> ret = client.validateConfig();
+
 		if (LOG.isDebugEnabled()) {
-			LOG.debug("<== RangerServiceAtlas.validateConfig Response : ("+ responseMap + " )");
+			LOG.debug("<== RangerServiceAtlas.validateConfig(): "+ ret );
 		}
-		return responseMap;
+
+		return ret;
 	}
 
 	@Override
 	public List<String> lookupResource(ResourceLookupContext context)throws Exception {
-		List<String> ret = new ArrayList<String>();
-		String serviceName = getServiceName();
-		Map<String, String> configs = getConfigs();
 		if (LOG.isDebugEnabled()) {
-			LOG.debug("==> RangerServiceAtlas.lookupResource Context: ("+ context + ")");
+			LOG.debug("==> RangerServiceAtlas.lookupResource("+ context + ")");
 		}
-		if (context != null) {
-			try {
-				ret = AtlasResourceMgr.getAtlasResources(serviceName, configs,context);
-			} catch (Exception e) {
-				LOG.error("<==RangerServiceAtlas.lookupResource Error : " + e);
-				throw e;
-			}
-		}
+
+		AtlasServiceClient client = new AtlasServiceClient(getServiceName(), configs);
+
+		List<String> ret = client.lookupResource(context);
+
 		if (LOG.isDebugEnabled()) {
-			LOG.debug("<== RangerServiceAtlas.lookupResource Response: (" + ret+ ")");
+			LOG.debug("<== RangerServiceAtlas.lookupResource("+ context + "): " + ret);
 		}
+
 		return ret;
 	}
 
@@ -116,4 +144,295 @@ public class RangerServiceAtlas extends RangerBaseService {
         }
         return ret;
     }
+
+    private static class AtlasServiceClient extends BaseClient {
+		private static final String[] TYPE_CATEGORIES = new String[] { "classification", "enum", "entity", "relationship", "struct" };
+
+		Map<String, List<String>> typesDef = new HashMap<>();
+
+		public AtlasServiceClient(String serviceName, Map<String, String> serviceConfig) {
+			super(serviceName, serviceConfig);
+		}
+
+		public Map<String, Object> validateConfig() {
+			Map<String, Object> ret = new HashMap<>();
+
+			loginToAtlas(Client.create());
+
+			BaseClient.generateResponseDataMap(true, "ConnectionTest Successful", "ConnectionTest Successful", null, null, ret);
+
+			return ret;
+		}
+
+		public List<String> lookupResource(ResourceLookupContext lookupContext) {
+			final List<String> ret           = new ArrayList<>();
+			final String       userInput     = lookupContext.getUserInput();
+			final List<String> currentValues = lookupContext.getResources().get(lookupContext.getResourceName());
+
+			switch(lookupContext.getResourceName()) {
+				case RESOURCE_TYPE_CATEGORY: {
+					for (String typeCategory : TYPE_CATEGORIES) {
+						addIfStartsWithAndNotExcluded(ret, typeCategory, userInput, currentValues);
+					}
+				}
+				break;
+
+				case RESOURCE_TYPE_NAME: {
+					refreshTypesDefs();
+
+					final List<String> typeCategories = lookupContext.getResources().get(RESOURCE_TYPE_CATEGORY);
+
+					if (emptyOrContainsMatch(typeCategories, TYPE_CLASSIFICATION)) {
+						addIfStartsWithAndNotExcluded(ret, typesDef.get(TYPE_CLASSIFICATION), userInput, currentValues);
+					}
+
+					if (emptyOrContainsMatch(typeCategories, TYPE_ENTITY)) {
+						addIfStartsWithAndNotExcluded(ret, typesDef.get(TYPE_ENTITY), userInput, currentValues);
+					}
+
+					if (emptyOrContainsMatch(typeCategories, TYPE_ENUM)) {
+						addIfStartsWithAndNotExcluded(ret, typesDef.get(TYPE_ENUM), userInput, currentValues);
+					}
+
+					if (emptyOrContainsMatch(typeCategories, TYPE_STRUCT)) {
+						addIfStartsWithAndNotExcluded(ret, typesDef.get(TYPE_STRUCT), userInput, currentValues);
+					}
+
+					if (emptyOrContainsMatch(typeCategories, TYPE_RELATIONSHIP)) {
+						addIfStartsWithAndNotExcluded(ret, typesDef.get(TYPE_RELATIONSHIP), userInput, currentValues);
+					}
+				}
+				break;
+
+				case RESOURCE_ENTITY_TYPE: {
+					refreshTypesDefs();
+
+					addIfStartsWithAndNotExcluded(ret, typesDef.get(TYPE_ENTITY), userInput, currentValues);
+				}
+				break;
+
+				case RESOURCE_ENTITY_CLASSIFICATION: {
+					refreshTypesDefs();
+
+					addIfStartsWithAndNotExcluded(ret, typesDef.get(TYPE_CLASSIFICATION), userInput, currentValues);
+				}
+				break;
+
+				default: {
+					ret.add(lookupContext.getResourceName());
+				}
+			}
+
+			return ret;
+		}
+
+		private ClientResponse loginToAtlas(Client client) {
+			ClientResponse ret      = null;
+			HadoopException excp     = null;
+			String          loginUrl = null;
+
+			for (String atlasUrl : getAtlasUrls()) {
+				try {
+					loginUrl = atlasUrl + URL_LOGIN;
+
+					WebResource                    webResource = client.resource(loginUrl);
+					MultivaluedMap<String, String> formData    = new MultivaluedMapImpl();
+					String                         password    = null;
+
+					try {
+						password = PasswordUtils.decryptPassword(getPassword());
+					} catch (Exception ex) {
+						LOG.info("Password decryption failed; trying Atlas connection with received password string");
+					}
+
+					if (password == null) {
+						password = getPassword();
+					}
+
+					formData.add("j_username", getUserName());
+					formData.add("j_password", password);
+
+					try {
+						ret = webResource.type(WEB_RESOURCE_CONTENT_TYPE).post(ClientResponse.class, formData);
+					} catch (Exception e) {
+						LOG.error("failed to login to Atlas at " + loginUrl, e);
+					}
+
+					if (ret != null) {
+						break;
+					}
+				} catch (Throwable t) {
+					String msgDesc = "Exception while login to Atlas at : " + loginUrl;
+
+					LOG.error(msgDesc, t);
+
+					excp = new HadoopException(msgDesc, t);
+					excp.generateResponseDataMap(false, BaseClient.getMessage(t), msgDesc + CONNECTION_ERROR_MSG, null, null);
+				}
+			}
+
+			if (ret == null) {
+				if (excp == null) {
+					String msgDesc = "Exception while login to Atlas at : " + loginUrl;
+
+					excp = new HadoopException(msgDesc);
+					excp.generateResponseDataMap(false, "", msgDesc + CONNECTION_ERROR_MSG, null, null);
+				}
+
+				throw excp;
+			}
+
+			return ret;
+		}
+
+		private boolean refreshTypesDefs() {
+			boolean ret = false;
+
+			Subject subj = getLoginSubject();
+
+			if (subj == null) {
+				return ret;
+			}
+
+			Map<String, List<String>> typesDef = Subject.doAs(subj, new PrivilegedAction<Map<String, List<String>>>() {
+				@Override
+				public Map<String, List<String>> run() {
+					Map<String, List<String>> ret  = null;
+
+					for (String atlasUrl : getAtlasUrls()) {
+						Client client = null;
+
+						try {
+							client = Client.create();
+
+							ClientResponse      loginResponse = loginToAtlas(client);
+							WebResource         webResource   = client.resource(atlasUrl + URL_GET_TYPESDEF_HEADERS);
+							WebResource.Builder builder       = webResource.getRequestBuilder();
+
+							for (NewCookie cook : loginResponse.getCookies()) {
+								builder = builder.cookie(cook);
+							}
+
+							ClientResponse response = builder.get(ClientResponse.class);
+
+							if (response != null) {
+								String jsonString = response.getEntity(String.class);
+								Gson   gson       = new Gson();
+								List   types      = gson.fromJson(jsonString, List.class);
+
+								ret = new HashMap<>();
+
+								for (Object type : types) {
+									if (type instanceof Map) {
+										Map typeDef = (Map) type;
+
+										Object name     = typeDef.get("name");
+										Object category = typeDef.get("category");
+
+										if (name != null && category != null) {
+											String       strCategory  = category.toString().toLowerCase();
+											List<String> categoryList = ret.get(strCategory);
+
+											if (categoryList == null) {
+												categoryList = new ArrayList<>();
+
+												ret.put(strCategory, categoryList);
+											}
+
+											categoryList.add(name.toString());
+										}
+									}
+								}
+
+								break;
+							}
+						} catch (Throwable t) {
+							String msgDesc = "Exception while getting Atlas Resource List.";
+							LOG.error(msgDesc, t);
+						} finally {
+							if (client != null) {
+								client.destroy();
+							}
+						}
+					}
+
+					return ret;
+				}
+			});
+
+			if (typesDef != null) {
+				this.typesDef = typesDef;
+				ret = true;
+			}
+
+			return ret;
+		}
+
+		String[] getAtlasUrls() {
+			String urlString = connectionProperties.get(CONFIG_REST_ADDRESS);
+
+			String[] ret = urlString == null ? new String[0] : urlString.split(",");
+
+			// remove separator at the end
+			for (int i = 0; i < ret.length; i++) {
+				String url = ret[i];
+
+				while (url.length() > 0 && url.charAt(url.length() - 1) == '/') {
+					url = url.substring(0, url.length() - 1);
+				}
+
+				ret[i] = url;
+			}
+
+			return ret;
+		}
+
+		String getUserName() {
+			return connectionProperties.get(CONFIG_USERNAME);
+		}
+
+		String getPassword() {
+			return connectionProperties.get(CONFIG_PASSWORD);
+		}
+
+		boolean emptyOrContainsMatch(List<String> list, String value) {
+			if (list == null || list.isEmpty()) {
+				return true;
+			}
+
+			for (String item : list) {
+				if (StringUtils.equalsIgnoreCase(item, value) || FilenameUtils.wildcardMatch(value, item, IOCase.INSENSITIVE)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		void addIfStartsWithAndNotExcluded(List<String> list, List<String> values, String prefix, List<String> excludeList) {
+			if (values == null || list == null) {
+				return;
+			}
+
+			for (String value : values) {
+				addIfStartsWithAndNotExcluded(list, value, prefix, excludeList);
+			}
+		}
+
+		void addIfStartsWithAndNotExcluded(List<String> list, String value, String prefix, List<String> excludeList) {
+			if (value == null || list == null) {
+				return;
+			}
+
+			if (prefix != null && !value.startsWith(prefix)) {
+				return;
+			}
+
+			if (excludeList != null && excludeList.contains(value)) {
+				return;
+			}
+
+			list.add(value);
+		}
+	}
 }
