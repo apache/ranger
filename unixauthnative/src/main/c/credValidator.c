c9b727d6201e0ca9e899be9c253e26a86af370b1
Zsombor Gegesy <zsombor@apache.org>
2018-12-07 10:47:53 +0100

    RANGER-2307: Better error message, and a NULL check for the native code

--- unixauthnative/src/main/c/credValidator.c
+++ unixauthnative/src/main/c/credValidator.c
@@ -22,6 +22,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <crypt.h>
+#include <errno.h>
 
 #define STRLEN 64
 
@@ -48,11 +49,15 @@ int main(int ac, char **av, char **ev)
 	spwd = getspnam(pwp->pw_name) ;
 
 	if (spwd == (struct spwd *)NULL) {
-		fprintf(stdout, "FAILED: unable to get (shadow) password for %s\n", username) ;
+		fprintf(stdout, "FAILED: unable to get (shadow) password for '%s', because '%s'\n", username, strerror(errno));
 		exit(1) ;
 	}
 	else {
 		char *gen = crypt(password,spwd->sp_pwdp) ;
+		if (gen == (char *)NULL) {
+			fprintf(stdout, "FAILED: crypt failed with: '%s'\n", strerror(errno));
+			exit(1);
+		}
 		if (strcmp(spwd->sp_pwdp,gen) == 0) {
 			fprintf(stdout, "OK:\n") ;
 			exit(0);
