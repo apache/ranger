d34d024e8afa64e8b5865eb3284ecf6a57928b30
rmani <rmani@hortonworks.com>
2019-04-11 22:33:10 -0700

    RANGER-2224:'drop temporary function <udf>' command should be handled by 'global' resource and 'Temorary UDF Admin' permission
    Signed-off-by: rmani <rmani@hortonworks.com>

--- hive-agent/src/main/java/org/apache/ranger/authorization/hive/authorizer/RangerHiveAuthorizer.java
+++ hive-agent/src/main/java/org/apache/ranger/authorization/hive/authorizer/RangerHiveAuthorizer.java
@@ -912,10 +912,7 @@ public class RangerHiveAuthorizer extends RangerHiveAuthorizerBase {
 
 			case FUNCTION:
 				objType = HiveObjectType.FUNCTION;
-				if (hiveOpTypeName.contains("createfunction") &&
-					StringUtils.isEmpty(hiveObj.getDbname())) {
-					// This happens for temp udf function and will use
-					// global resource policy in ranger for auth
+				if (isTempUDFOperation(hiveOpTypeName, hiveObj)) {
 					objType = HiveObjectType.GLOBAL;
 				}
 			break;
@@ -1962,6 +1959,17 @@ public class RangerHiveAuthorizer extends RangerHiveAuthorizerBase {
 			result = accessResult.getResult();
 		}
 		return result;
+	}
+
+	private boolean isTempUDFOperation(String hiveOpTypeName, HivePrivilegeObject hiveObj) {
+		boolean ret = false;
+		if ((hiveOpTypeName.contains("createfunction") || hiveOpTypeName.contains("dropfunction")) &&
+			StringUtils.isEmpty(hiveObj.getDbname())) {
+			// This happens for temp udf function and will use
+			// global resource policy in ranger for auth
+			ret = true;
+		}
+		return ret;
 	}
 
 	private String toString(HiveOperationType         hiveOpType,
