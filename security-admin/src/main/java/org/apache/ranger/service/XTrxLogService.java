40d935cd635ea23596590f444c3c6ce71474bbb9
maheshbandal <maheshbandal15@gmail.com>
2020-04-10 16:45:22 +0530

    RANGER-2790 : Import start and import end are not in sequence
    Signed-off-by: Mehul Parikh <mehul@apache.org>

--- security-admin/src/main/java/org/apache/ranger/service/XTrxLogService.java
+++ security-admin/src/main/java/org/apache/ranger/service/XTrxLogService.java
@@ -94,9 +94,9 @@ public class XTrxLogService extends XTrxLogServiceBase<XXTrxLog, VXTrxLog> {
 
 		selectCQ.where(predicate);
 		if ("asc".equalsIgnoreCase(searchCriteria.getSortType())) {
-			selectCQ.orderBy(criteriaBuilder.asc(rootEntityType.get("createTime")));
+			selectCQ.orderBy(criteriaBuilder.asc(rootEntityType.get("id")));
 		} else {
-			selectCQ.orderBy(criteriaBuilder.desc(rootEntityType.get("createTime")));
+			selectCQ.orderBy(criteriaBuilder.desc(rootEntityType.get("id")));
 		}
 		int startIndex = searchCriteria.getStartIndex();
 		int pageSize = searchCriteria.getMaxRows();
