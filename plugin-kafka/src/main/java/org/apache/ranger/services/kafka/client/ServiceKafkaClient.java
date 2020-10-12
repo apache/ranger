3f21afc42225a902bf13111b883e68d3c0e6a767
Ramesh Mani <rmani@cloudera.com>
2020-10-12 09:17:34 -0700

    RANGER-3001:Update Ranger KafkaClient to use Kafka AdminClient API instead of Zookeeper

--- plugin-kafka/src/main/java/org/apache/ranger/services/kafka/client/ServiceKafkaClient.java
+++ plugin-kafka/src/main/java/org/apache/ranger/services/kafka/client/ServiceKafkaClient.java
@@ -19,24 +19,24 @@
 
 package org.apache.ranger.services.kafka.client;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.kafka.common.utils.Time;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.apache.log4j.Logger;
 import org.apache.ranger.plugin.client.BaseClient;
 import org.apache.ranger.plugin.service.ResourceLookupContext;
 import org.apache.ranger.plugin.util.TimedEventUtil;
-
-import kafka.zk.KafkaZkClient;
-import kafka.zookeeper.ZooKeeperClient;
-import scala.Option;
-import scala.collection.Iterator;
 
 public class ServiceKafkaClient {
 	private static final Logger LOG = Logger.getLogger(ServiceKafkaClient.class);
@@ -45,21 +45,32 @@ public class ServiceKafkaClient {
 		TOPIC
 	}
 
-	String serviceName = null;
-	String zookeeperConnect = null;
+	String serviceName;
+	Map<String,String > configs;
 	private static final String errMessage = " You can still save the repository and start creating "
 			+ "policies, but you would not be able to use autocomplete for "
 			+ "resource names. Check server logs for more info.";
 
-	private static final String TOPIC_KEY = "topic";
-	private static final long LOOKUP_TIMEOUT_SEC = 5;
+	private static final String TOPIC_KEY				= "topic";
+	private static final long   LOOKUP_TIMEOUT_SEC		= 5;
+	private static final String KEY_SASL_MECHANISM		= "sasl.mechanism";
+	private static final String KEY_SASL_JAAS_CONFIG	= "sasl.jaas.config";
+	private static final String KEY_KAFKA_KEYTAB		= "kafka.keytab";
+	private static final String KEY_KAFKA_PRINCIPAL		= "kafka.principal";
+	private static final String JAAS_KRB5_MODULE		= "com.sun.security.auth.module.Krb5LoginModule required";
+	private static final String JAAS_USE_KEYTAB			= "useKeyTab=true";
+	private static final String JAAS_KEYTAB				= "keyTab=\"";
+	private static final String JAAS_STOKE_KEY			= "storeKey=true";
+	private static final String JAAS_SERVICE_NAME		= "serviceName=kafka";
+	private static final String JAAS_USER_TICKET_CACHE	= "useTicketCache=false";
+	private static final String JAAS_PRINCIPAL			= "principal=\"";
 
-	public ServiceKafkaClient(String serviceName, String zookeeperConnect) {
+	public ServiceKafkaClient(String serviceName, Map<String,String> configs) {
 		this.serviceName = serviceName;
-		this.zookeeperConnect = zookeeperConnect;
+		this.configs = configs;
 	}
 
-	public Map<String, Object> connectionTest() throws Exception {
+	public Map<String, Object> connectionTest() {
 		String errMsg = errMessage;
 		Map<String, Object> responseData = new HashMap<String, Object>();
 		try {
@@ -69,7 +80,7 @@ public class ServiceKafkaClient {
 			String successMsg = "ConnectionTest Successful";
 			BaseClient.generateResponseDataMap(true, successMsg,
 					successMsg, null, null, responseData);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			LOG.error("Error connecting to Kafka. kafkaClient=" + this, e);
 			String failureMsg = "Unable to connect to Kafka instance."
 					+ e.getMessage();
@@ -84,22 +95,41 @@ public class ServiceKafkaClient {
 
 		int sessionTimeout = 5000;
 		int connectionTimeout = 10000;
-		ZooKeeperClient zookeeperClient = new ZooKeeperClient(zookeeperConnect, sessionTimeout, connectionTimeout,
-				1, Time.SYSTEM, "kafka.server", "SessionExpireListener", Option.empty());
-		try (KafkaZkClient kafkaZkClient = new KafkaZkClient(zookeeperClient, true, Time.SYSTEM)) {
-			Iterator<String> iter = kafkaZkClient.getAllTopicsInCluster().iterator();
-			while (iter.hasNext()) {
-				String topic = iter.next();
-				if (ignoreTopicList == null || !ignoreTopicList.contains(topic)) {
-					ret.add(topic);
+		AdminClient adminClient = null;
+
+		try {
+			Properties props = new Properties();
+			props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, configs.get(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG));
+			props.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, configs.get(AdminClientConfig.SECURITY_PROTOCOL_CONFIG));
+			props.put(KEY_SASL_MECHANISM, configs.get(KEY_SASL_MECHANISM));
+			props.put(KEY_SASL_JAAS_CONFIG, getJAASConfig(configs));
+			props.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, getIntProperty(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, sessionTimeout));
+			props.put(AdminClientConfig.CONNECTIONS_MAX_IDLE_MS_CONFIG, getIntProperty(AdminClientConfig.CONNECTIONS_MAX_IDLE_MS_CONFIG, connectionTimeout));
+			adminClient = KafkaAdminClient.create(props);
+			ListTopicsResult listTopicsResult = adminClient.listTopics();
+			if (listTopicsResult != null) {
+				Collection<TopicListing> topicListings = listTopicsResult.listings().get();
+				for (TopicListing topicListing : topicListings) {
+					String topicName = topicListing.name();
+					if (ignoreTopicList == null || !ignoreTopicList.contains(topicName)) {
+						ret.add(topicName);
+					}
 				}
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (adminClient != null) {
+				adminClient.close();
 			}
 		}
 		return ret;
 	}
 
+
+
 	/**
-	 * @param serviceName
+	 * @param context
 	 * @param context
 	 * @return
 	 */
@@ -124,11 +154,11 @@ public class ServiceKafkaClient {
 				topicList = resourceMap.get(TOPIC_KEY);
 			}
 			switch (resource.trim().toLowerCase()) {
-			case TOPIC_KEY:
-				lookupResource = RESOURCE_TYPE.TOPIC;
-				break;
-			default:
-				break;
+				case TOPIC_KEY:
+					lookupResource = RESOURCE_TYPE.TOPIC;
+					break;
+				default:
+					break;
 			}
 		}
 
@@ -182,7 +212,34 @@ public class ServiceKafkaClient {
 	@Override
 	public String toString() {
 		return "ServiceKafkaClient [serviceName=" + serviceName
-				+ ", zookeeperConnect=" + zookeeperConnect + "]";
+				+ ", configs=" + configs + "]";
+	}
+
+	private Integer getIntProperty(String key, int defaultValue) {
+		if (key == null) {
+			return defaultValue;
+		}
+		String rtrnVal = configs.get(key);
+		if (rtrnVal == null) {
+			return defaultValue;
+		}
+		return Integer.valueOf(rtrnVal);
+	}
+
+	private String getJAASConfig(Map<String,String> configs){
+		String jaasConfig =  new StringBuilder()
+				.append(JAAS_KRB5_MODULE).append(" ")
+				.append(JAAS_USE_KEYTAB).append(" ")
+				.append(JAAS_KEYTAB).append(configs.get(KEY_KAFKA_KEYTAB)).append("\"").append(" ")
+				.append(JAAS_STOKE_KEY).append(" ")
+				.append(JAAS_USER_TICKET_CACHE).append(" ")
+				.append(JAAS_SERVICE_NAME).append(" ")
+				.append(JAAS_PRINCIPAL).append(configs.get(KEY_KAFKA_PRINCIPAL)).append("\";")
+				.toString();
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("KafkaClient JAAS: " + jaasConfig);
+		}
+		return jaasConfig;
 	}
 
 }
