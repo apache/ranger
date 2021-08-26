fbc08b35b003da4bc42748636aa0b91c8734f903
Madhan Neethiraj <madhan@apache.org>
2021-08-25 23:14:14 -0700

    Revert "RANGER-3377: HDFS plugin performance improvement - RangerHdfsResource.getAsString()"
    This reverts commit b0e970f21f7b2bd3f591e291684014f92434a2d3.

--- hdfs-agent/src/main/java/org/apache/ranger/authorization/hadoop/RangerHdfsAuthorizer.java
+++ hdfs-agent/src/main/java/org/apache/ranger/authorization/hadoop/RangerHdfsAuthorizer.java
@@ -907,19 +907,6 @@ class RangerHdfsResource extends RangerAccessResourceImpl {
 		super.setValue(RangerHdfsAuthorizer.KEY_RESOURCE_PATH, path);
 		super.setOwnerUser(owner);
 	}
-
-	@Override
-	public String getAsString() {
-		String ret = super.getStringifiedValue();
-
-		if (ret == null) {
-			ret = Objects.toString(super.getValue(RangerHdfsAuthorizer.KEY_RESOURCE_PATH));
-
-			super.setStringifiedValue(ret);
-		}
-
-		return ret;
-	}
 }
 
 class RangerHdfsAccessRequest extends RangerAccessRequestImpl {
