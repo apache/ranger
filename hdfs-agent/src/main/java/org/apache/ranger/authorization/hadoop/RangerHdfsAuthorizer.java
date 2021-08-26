88d4030531dd673fb8f8899c274bc56852e625b5
cao zhiqiang <lfxy@163.com>
2021-08-25 22:16:39 -0700

    RANGER-3378: HDFS plugin performance improvement - RangerHdfsResource.getAsString()
    Signed-off-by: Madhan Neethiraj <madhan@apache.org>
    (cherry picked from commit b0e970f21f7b2bd3f591e291684014f92434a2d3)

--- hdfs-agent/src/main/java/org/apache/ranger/authorization/hadoop/RangerHdfsAuthorizer.java
+++ hdfs-agent/src/main/java/org/apache/ranger/authorization/hadoop/RangerHdfsAuthorizer.java
@@ -30,6 +30,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.Stack;
 
@@ -813,6 +814,19 @@ class RangerHdfsResource extends RangerAccessResourceImpl {
 	public RangerHdfsResource(String path, String owner) {
 		super.setValue(RangerHdfsAuthorizer.KEY_RESOURCE_PATH, path);
 		super.setOwnerUser(owner);
+	}
+
+	@Override
+	public String getAsString() {
+		String ret = super.getStringifiedValue();
+
+		if (ret == null) {
+			ret = Objects.toString(super.getValue(RangerHdfsAuthorizer.KEY_RESOURCE_PATH));
+
+			super.setStringifiedValue(ret);
+		}
+
+		return ret;
 	}
 }
 
