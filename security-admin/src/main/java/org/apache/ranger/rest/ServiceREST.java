fcc4928de588bfcc974b91853e340ec5dcfcc7fc
egor-ryashin <egor.ryashin@rilldata.com>
2021-10-15 18:17:18 +0300

    Ranger hide security zone services

--- security-admin/src/main/java/org/apache/ranger/rest/ServiceREST.java
+++ security-admin/src/main/java/org/apache/ranger/rest/ServiceREST.java
@@ -33,6 +33,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
@@ -194,6 +195,9 @@ public class ServiceREST {
 
 	@Autowired
 	XUserService xUserService;
+
+	@Autowired
+	SecurityZoneDBStore securityZoneDBStore;
 
 	@Autowired
 	AssetMgr assetMgr;
@@ -1017,9 +1021,13 @@ public class ServiceREST {
 										RangerConstants.ROLE_USER)) {
 							
 							List<RangerService> updateServiceList = new ArrayList<RangerService>();
+							List<RangerSecurityZone> securityZones = zoneStore.getSecurityZones(null);
+							Set<String> serviceNames = securityZones.stream().filter(sz -> sz.getAdminUsers().contains(loggedInVXUser.getName()))
+																											.flatMap(sz -> sz.getServices().keySet().stream()).collect(
+									Collectors.toSet());
 							for(RangerService rangerService : paginatedSvcs.getList()){
-								
-								if(rangerService != null){
+
+								if(rangerService != null && serviceNames.contains(rangerService.getName())){
 									updateServiceList.add(hideCriticalServiceDetailsForRoleUser(rangerService));
 								}
 							}
