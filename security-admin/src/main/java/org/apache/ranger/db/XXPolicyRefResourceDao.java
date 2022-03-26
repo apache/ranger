d74f3fced96459ec8bc8f487b2138e4d680d3b49
Xuze Yang <xuze_yang@163.com>
2022-03-26 09:47:26 +0800

    RANGER-3681: Ranger Database deadlock when createPolicy is running parallel
    Signed-off-by: Madhan Neethiraj <madhan@apache.org>

--- security-admin/src/main/java/org/apache/ranger/db/XXPolicyRefResourceDao.java
+++ security-admin/src/main/java/org/apache/ranger/db/XXPolicyRefResourceDao.java
@@ -25,6 +25,7 @@ import java.util.List;
 
 import javax.persistence.NoResultException;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.ranger.biz.RangerPolicyRetriever;
 import org.apache.ranger.common.db.BaseDao;
 import org.apache.ranger.entity.XXPolicyRefResource;
@@ -66,9 +67,22 @@ public class XXPolicyRefResourceDao extends BaseDao<XXPolicyRefResource>{
 		if(policyId == null) {
 			return;
 		}
+
+		// First select ids according to policyId, then delete records according to ids
+		// The purpose of dividing the delete sql into these two steps is to avoid deadlocks at rr isolation level
+		List<Long> ids = getEntityManager()
+				.createNamedQuery("XXPolicyRefResource.findIdsByPolicyId", Long.class)
+				.setParameter("policyId", policyId)
+				.getResultList();
+
+		if (CollectionUtils.isEmpty(ids)) {
+			return;
+		}
+
 		getEntityManager()
-			.createNamedQuery("XXPolicyRefResource.deleteByPolicyId", tClass)
-			.setParameter("policyId", policyId).executeUpdate();
+				.createNamedQuery("XXPolicyRefResource.deleteByIds", tClass)
+				.setParameter("ids", ids).executeUpdate();
+
 	}
 
 	 @SuppressWarnings("unchecked")
