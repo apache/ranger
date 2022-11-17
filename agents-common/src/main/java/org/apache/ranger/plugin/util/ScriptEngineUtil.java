9b22848442873b50728c825083b5377d4c56fd6d
Madhan Neethiraj <madhan@apache.org>
2022-11-17 14:17:30 -0800

    RANGER-3970: expression evaluation to use Graal engine when Nashorn is not available

--- agents-common/src/main/java/org/apache/ranger/plugin/util/ScriptEngineUtil.java
+++ agents-common/src/main/java/org/apache/ranger/plugin/util/ScriptEngineUtil.java
@@ -26,13 +26,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.script.ScriptEngine;
-import jdk.nashorn.api.scripting.ClassFilter;
-import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+
 
 public class ScriptEngineUtil {
     private static final Logger LOG = LoggerFactory.getLogger(RangerScriptConditionEvaluator.class);
 
-    private static final String[] SCRIPT_ENGINE_ARGS = new String[] { "--no-java", "--no-syntax-extensions" };
+    private static volatile ScriptEngineCreator SCRIPT_ENGINE_CREATOR             = null;
+    private static volatile boolean             SCRIPT_ENGINE_CREATOR_INITIALIZED = false;
 
     // for backward compatibility with any plugin that might use this API
     public static ScriptEngine createScriptEngine(String engineName, String serviceType) {
@@ -48,71 +48,112 @@ public class ScriptEngineUtil {
             LOG.debug("==> ScriptEngineUtil.createScriptEngine(serviceType=" + serviceType + ")");
         }
 
-        ScriptEngine ret = getScriptEngine(null);
+        ScriptEngine        ret     = null;
+        ScriptEngineCreator creator = getScriptEngineCreator(serviceType);
 
-        if (ret != null) {
-            LOG.debug("Created script-engine in current class-loader");
-        } else {
-            LOG.warn("Failed to create script-engine in current class-loader. Will try plugin-class-loader for service-type:[" + serviceType + "]");
+        if (creator != null) {
+            ret = creator.getScriptEngine(null);
 
-            ClassLoader prevActiveClassLoader = null;
+            if (ret == null) {
+                ClassLoader pluginClsLoader = getPrevActiveClassLoader(serviceType);
 
-            try {
-                RangerPluginClassLoader pluginClassLoader = RangerPluginClassLoader.getInstance(serviceType, null);
-
-                if (pluginClassLoader != null) {
-                    prevActiveClassLoader = pluginClassLoader.getPrevActiveClassLoader();
-                } else {
-                    LOG.error("Cannot get script-engine from null plugin-class-loader");
+                if (pluginClsLoader != null) {
+                    ret = creator.getScriptEngine(pluginClsLoader);
                 }
-            } catch (Throwable exp) {
-                LOG.error("RangerScriptConditionEvaluator.init() failed", exp);
             }
+        } else {
+            LOG.info("createScriptEngine(serviceType={}): no engine creator found", serviceType);
+        }
 
-            if (prevActiveClassLoader != null) {
-                ret = getScriptEngine(prevActiveClassLoader);
-            }
+        if (ret == null) {
+            LOG.warn("createScriptEngine(serviceType={}): failed to create script engine", serviceType);
         }
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("<== ScriptEngineUtil.createScriptEngine(serviceType=" + serviceType + ") : ret=" + ret);
+            LOG.debug("<== ScriptEngineUtil.createScriptEngine(serviceType={}): ret={}", serviceType, ret);
         }
+
         return ret;
     }
 
-    private static ScriptEngine getScriptEngine(ClassLoader clsLoader) {
-        ScriptEngine ret;
+    private static ScriptEngineCreator getScriptEngineCreator(String serviceType) {
+        boolean isInitialized = SCRIPT_ENGINE_CREATOR_INITIALIZED;
+
+        if (!isInitialized) {
+            synchronized (ScriptEngineUtil.class) {
+                isInitialized = SCRIPT_ENGINE_CREATOR_INITIALIZED;
+
+                if (!isInitialized) {
+                    initScriptEngineCreator(serviceType);
+                }
+
+                SCRIPT_ENGINE_CREATOR_INITIALIZED = true;
+            }
+        }
+
+        return SCRIPT_ENGINE_CREATOR;
+    }
+
+    private static void initScriptEngineCreator(String serviceType) {
+        String[] engineCreators = new String[] { "org.apache.ranger.plugin.util.NashornScriptEngineCreator",
+                                                 "org.apache.ranger.plugin.util.GraalScriptEngineCreator",
+                                                 "org.apache.ranger.plugin.util.JavaScriptEngineCreator"
+                                               };
+
+        for (String creatorClsName : engineCreators) {
+            ScriptEngineCreator creator = null;
+
+            try {
+                Class<ScriptEngineCreator> creatorClass = (Class<ScriptEngineCreator>) Class.forName(creatorClsName);
+
+                creator = creatorClass.newInstance();
+            } catch (Throwable t) {
+                LOG.warn("initScriptEngineCreator(): failed to instantiate engine creator {}", creatorClsName, t);
+            }
+
+            if (creator == null) {
+                continue;
+            }
+
+            ScriptEngine engine = creator.getScriptEngine(null);
+
+            if (engine == null) {
+                ClassLoader prevActiveClassLoader = getPrevActiveClassLoader(serviceType);
+
+                if (prevActiveClassLoader != null) {
+                    LOG.debug("initScriptEngineCreator(): trying to create engine using plugin-class-loader for service-type {}", serviceType);
+
+                    engine = creator.getScriptEngine(prevActiveClassLoader);
+
+                    if (engine == null) {
+                        LOG.warn("initScriptEngineCreator(): failed to create engine using plugin-class-loader by creator {}", creatorClsName);
+                    }
+                }
+            }
+
+            if (engine != null) {
+                SCRIPT_ENGINE_CREATOR = creator;
+
+                break;
+            }
+        }
+    }
+
+    private static ClassLoader getPrevActiveClassLoader(String serviceType) {
+        ClassLoader ret = null;
 
         try {
-            final NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
+            RangerPluginClassLoader pluginClassLoader = RangerPluginClassLoader.getInstance(serviceType, null);
 
-            if (clsLoader == null) {
-                clsLoader = Thread.currentThread().getContextClassLoader();
+            if (pluginClassLoader != null) {
+                ret = pluginClassLoader.getPrevActiveClassLoader();
+            } else {
+                LOG.debug("Cannot get plugin-class-loader for serviceType {}", serviceType);
             }
-
-            ret = factory.getScriptEngine(SCRIPT_ENGINE_ARGS, clsLoader, RangerClassFilter.INSTANCE);
-        } catch (Throwable t) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("ScriptEngineUtil.getScriptEngine(clsLoader={}): failed", clsLoader, t);
-            }
-
-            ret = null;
+        } catch (Throwable excp) {
+            LOG.debug("Failed to get plugin-class-loader for serviceType {}", serviceType, excp);
         }
 
         return ret;
-    }
-
-    private static class RangerClassFilter implements ClassFilter {
-        static final RangerClassFilter INSTANCE = new RangerClassFilter();
-
-        private RangerClassFilter() {
-        }
-
-        @Override
-        public boolean exposeToScripts(String className) {
-            LOG.warn("script blocked: attempt to use Java class {}", className);
-
-            return false;
-        }
     }
 }
