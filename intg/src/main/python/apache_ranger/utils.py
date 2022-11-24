28f3c09d1fb32bd72414b927f63b66100e7ea0dd
Madhan Neethiraj <madhan@apache.org>
2022-11-23 17:17:50 -0800

    RANGER-3982: updated Python client to support Ranger KMS REST APIs
    (cherry picked from commit d0c6bdb6eb193d849bbf0cea8d7c425c0901981d)

--- intg/src/main/python/apache_ranger/utils.py
+++ intg/src/main/python/apache_ranger/utils.py
@@ -87,8 +87,24 @@ class HttpMethod(enum.Enum):
 
 
 class HTTPStatus:
-    OK                    = 200
-    NO_CONTENT            = 204
-    NOT_FOUND             = 404
-    INTERNAL_SERVER_ERROR = 500
-    SERVICE_UNAVAILABLE   = 503
+    OK                     = 200
+    CREATED                = 201
+    ACCEPTED               = 202
+    NO_CONTENT             = 204
+    MOVED_PERMANENTLY      = 301
+    SEE_OTHER              = 303
+    NOT_MODIFIED           = 304
+    TEMPORARY_REDIRECT     = 307
+    BAD_REQUEST            = 400
+    UNAUTHORIZED           = 401
+    FORBIDDEN              = 403
+    NOT_FOUND              = 404
+    NOT_ACCEPTABLE         = 406
+    CONFLICT               = 409
+    GONE                   = 410
+    PRECONDITION_FAILED    = 412
+    UNSUPPORTED_MEDIA_TYPE = 415
+    INTERNAL_SERVER_ERROR  = 500
+    SERVICE_UNAVAILABLE    = 503
+
+
