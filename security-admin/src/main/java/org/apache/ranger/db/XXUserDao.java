c7ee11e8eb9c299e3a5fa9dc38a16dc93916a9fe
Madhan Neethiraj <madhan@apache.org>
2023-08-29 23:53:43 -0700

    RANGER-4387: optimize loading of userstore from database
    (cherry picked from commit 1cf5934dd3e4e10096af72bfb82253e8b81d2b8c)

--- security-admin/src/main/java/org/apache/ranger/db/XXUserDao.java
+++ security-admin/src/main/java/org/apache/ranger/db/XXUserDao.java
@@ -25,6 +25,12 @@ import org.apache.commons.collections.ListUtils;
 import org.apache.ranger.common.db.BaseDao;
 import org.apache.ranger.entity.XXUser;
 import org.apache.ranger.plugin.model.RangerPrincipal;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ranger.common.db.BaseDao;
+import org.apache.ranger.entity.XXUser;
+import org.apache.ranger.plugin.model.UserInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -36,9 +42,12 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
 @Service
 public class XXUserDao extends BaseDao<XXUser> {
-	private static final Logger logger = LoggerFactory.getLogger(XXResourceDao.class);
+	private static final Logger logger = LoggerFactory.getLogger(XXUserDao.class);
+
+	private static final Gson gsonBuilder = new GsonBuilder().create();
 
 	public XXUserDao(RangerDaoManagerBase daoManager) {
 		super(daoManager);
@@ -156,5 +165,43 @@ public class XXUserDao extends BaseDao<XXUser> {
 			ret = ListUtils.EMPTY_LIST;
 		}
 		return ret;
+	}
+
+	public List<UserInfo> getAllUsersInfo() {
+		List<UserInfo> ret = new ArrayList<>();
+
+		try {
+			List<Object[]> rows = getEntityManager().createNamedQuery("XXUser.getAllUsersInfo", Object[].class).getResultList();
+
+			if (rows != null) {
+				for (Object[] row : rows) {
+
+					ret.add(toUserInfo(row));
+				}
+			}
+		} catch (NoResultException excp) {
+			if (logger.isDebugEnabled()) {
+				logger.debug(excp.getMessage());
+			}
+		}
+
+		return ret;
+	}
+
+	private UserInfo toUserInfo(Object[] row) {
+		String              name        = (String) row[0];
+		String              description = (String) row[1];
+		String              attributes  = (String) row[2];
+		Map<String, String> attrMap     = null;
+
+		if (StringUtils.isNotBlank(attributes)) {
+			try {
+				attrMap = gsonBuilder.fromJson(attributes, Map.class);
+			} catch (Exception excp) {
+				// ignore
+			}
+		}
+
+		return new UserInfo(name, description, attrMap);
 	}
 }
