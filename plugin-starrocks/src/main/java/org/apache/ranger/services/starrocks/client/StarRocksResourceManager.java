06c1ed8de109953bf7f45642d1ed33dd64e584be
HangyuanLiu <460660596@qq.com>
2023-09-15 14:25:09 +0800

    Support Ranger StarRocks Plugin

--- plugin-starrocks/src/main/java/org/apache/ranger/services/starrocks/client/StarRocksResourceManager.java
+++ plugin-starrocks/src/main/java/org/apache/ranger/services/starrocks/client/StarRocksResourceManager.java
@@ -45,7 +45,7 @@ public class StarRocksResourceManager {
     private static final Logger LOG = LoggerFactory.getLogger(StarRocksResourceManager.class);
 
     public static Map<String, Object> connectionTest(String serviceName, Map<String, String> configs) throws Exception {
-        Map<String, Object> ret = null;
+        Map<String, Object> ret;
 
         if (LOG.isDebugEnabled()) {
             LOG.debug("==> StarRocksResourceManager.connectionTest() ServiceName: " + serviceName + " Configs: " + configs);
@@ -65,141 +65,6 @@ public class StarRocksResourceManager {
         return ret;
     }
 
-    public static List<String> getStarRocksResources2(String serviceName, String serviceType, Map<String, String> configs,
-                                                      ResourceLookupContext context) throws Exception {
-
-        String userInput = context.getUserInput();
-        String resource = context.getResourceName();
-        Map<String, List<String>> resourceMap = context.getResources();
-        List<String> resultList = null;
-        List<String> catalogList = null;
-        List<String> databaseList = null;
-        List<String> tableList = null;
-        List<String> columnList = null;
-
-        String catalogName = null;
-        String databaseName = null;
-        String tableName = null;
-        String columnName = null;
-
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("<== StarRocksResourceManager.getStarRocksResources() UserInput: \"" + userInput + "\" resource : " +
-                    resource +
-                    " resourceMap: " + resourceMap);
-        }
-
-        LOG.error("UserInput : " + userInput + " ," + "Resource : " + resource);
-
-        if (userInput == null) {
-            LOG.error("UserInput is null");
-        }
-
-        if (userInput == null || userInput.isEmpty()) {
-            userInput = "*";
-        }
-
-        if (resource != null) {
-            if (resourceMap != null && !resourceMap.isEmpty()) {
-                catalogList = resourceMap.get(CATALOG);
-                databaseList = resourceMap.get(DATABASE);
-                tableList = resourceMap.get(TABLE);
-                columnList = resourceMap.get(COLUMN);
-            }
-            switch (resource.trim().toLowerCase()) {
-                case CATALOG:
-                    catalogName = userInput;
-                    break;
-                case DATABASE:
-                    databaseName = userInput;
-                case TABLE:
-                    tableName = userInput;
-                    break;
-                case COLUMN:
-                    columnName = userInput;
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        if (serviceName != null) {
-            try {
-
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug(
-                            "==> StarRocksResourceManager.getStarRocksResources() UserInput: \"" + userInput + "\" configs: " +
-                                    configs +
-                                    " catalogList: " + catalogList + " tableList: "
-                                    + tableList + " columnList: " + columnList);
-                }
-
-                final StarRocksClient starRocksClient =
-                        new StarRocksConnectionManager().getStarRocksConnection(serviceName, serviceType, configs);
-
-                Callable<List<String>> callableObj = null;
-
-                final String finalCatalogName;
-                final String finalSchemaName;
-                final String finalTableName;
-                final String finalColumnName;
-
-                final List<String> finalCatalogList = catalogList;
-                final List<String> finalSchemaList = databaseList;
-                final List<String> finalTableList = tableList;
-                final List<String> finalColumnList = columnList;
-
-                if (starRocksClient != null) {
-                    if (catalogName != null) {
-                        finalCatalogName = catalogName;
-                        callableObj = new Callable<List<String>>() {
-                            @Override
-                            public List<String> call() throws Exception {
-                                return starRocksClient.getCatalogList(finalCatalogName, finalCatalogList);
-                            }
-                        };
-                    } else if (databaseName != null) {
-                        finalSchemaName = databaseName;
-                        callableObj = new Callable<List<String>>() {
-                            @Override
-                            public List<String> call() throws Exception {
-                                return starRocksClient.getDatabaseList(finalSchemaName, finalCatalogList, finalSchemaList);
-                            }
-                        };
-                    } else if (tableName != null) {
-                        finalTableName = tableName;
-                        callableObj = new Callable<List<String>>() {
-                            @Override
-                            public List<String> call() throws Exception {
-                                return starRocksClient.getTableList(finalTableName, finalCatalogList, finalSchemaList,
-                                        finalTableList);
-                            }
-                        };
-                    } else if (columnName != null) {
-                        finalColumnName = columnName;
-                        callableObj = new Callable<List<String>>() {
-                            @Override
-                            public List<String> call() throws Exception {
-                                return starRocksClient.getColumnList(finalColumnName, finalCatalogList, finalSchemaList,
-                                        finalTableList, finalColumnList);
-                            }
-                        };
-                    }
-                    if (callableObj != null) {
-                        //synchronized (starRocksClient) {
-                            resultList = TimedEventUtil.timedTask(callableObj, 5, TimeUnit.SECONDS);
-                        //}
-                    } else {
-                        LOG.error("Could not initiate a StarRocks timedTask");
-                    }
-                }
-            } catch (Exception e) {
-                LOG.error("Unable to get StarRocks resource", e);
-                throw e;
-            }
-        }
-        return resultList;
-    }
-
     public static List<String> getStarRocksResources(String serviceName, String serviceType, Map<String, String> configs,
                                                      ResourceLookupContext context) throws Exception {
 
@@ -214,9 +79,6 @@ public class StarRocksResourceManager {
             LOG.debug("<== StarRocksResourceManager.getStarRocksResources() UserInput: \"" + userInput + "\" resourceName : " +
                     resourceName + " resourceMap: " + context.getResources());
         }
-
-        LOG.error("<== StarRocksResourceManager.getStarRocksResources() UserInput: \"" + userInput + "\" resourceName : " +
-                resourceName + " resourceMap: " + context.getResources());
 
         List<String> resultList = null;
         try {
@@ -349,43 +211,5 @@ public class StarRocksResourceManager {
         }
 
         return resultList;
-    }
-
-    private static Callable<List<String>> getCatalogResource(StarRocksClient starRocksClient, ResourceLookupContext context,
-                                                             String catalogName) {
-        Map<String, List<String>> resourceMap = context.getResources();
-        List<String> catalogList = resourceMap.get(CATALOG);
-
-        return () -> starRocksClient.getCatalogList(catalogName, catalogList);
-    }
-
-    private static Callable<List<String>> getDatabaseResource(StarRocksClient starRocksClient, ResourceLookupContext context,
-                                                              String dbName) {
-        Map<String, List<String>> resourceMap = context.getResources();
-        List<String> catalogList = resourceMap.get(CATALOG);
-        List<String> databaseList = resourceMap.get(DATABASE);
-
-        return () -> starRocksClient.getDatabaseList(dbName, catalogList, databaseList);
-    }
-
-    private static Callable<List<String>> getTableResource(StarRocksClient starRocksClient, ResourceLookupContext context,
-                                                           String tableName) {
-        Map<String, List<String>> resourceMap = context.getResources();
-        List<String> catalogList = resourceMap.get(CATALOG);
-        List<String> databaseList = resourceMap.get(DATABASE);
-        List<String> tableList = resourceMap.get(TABLE);
-
-        return () -> starRocksClient.getTableList(tableName, catalogList, databaseList, tableList);
-    }
-
-    private static Callable<List<String>> getColumnResource(StarRocksClient starRocksClient, ResourceLookupContext context,
-                                                            String columnName) {
-        Map<String, List<String>> resourceMap = context.getResources();
-        List<String> catalogList = resourceMap.get(CATALOG);
-        List<String> databaseList = resourceMap.get(DATABASE);
-        List<String> tableList = resourceMap.get(TABLE);
-        List<String> columnList = resourceMap.get(COLUMN);
-
-        return () -> starRocksClient.getColumnList(columnName, catalogList, databaseList, tableList, columnList);
     }
 }
