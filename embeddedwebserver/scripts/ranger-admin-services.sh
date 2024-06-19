1436ca3d2a4453dff5385424b3e15bfe616a2add
Fernando Arribas Jara <fernando.arribas.jara@gmail.com>
2024-06-19 21:20:29 +0200

    Checks if the GC log directory exists, if not, create it

--- embeddedwebserver/scripts/ranger-admin-services.sh
+++ embeddedwebserver/scripts/ranger-admin-services.sh
@@ -54,6 +54,19 @@ then
 	RANGER_ADMIN_LOG_DIR=${XAPOLICYMGR_EWS_DIR}/logs
 fi
 
+LOG_GC_DIR=${XAPOLICYMGR_EWS_DIR}/logs/
+
+# Checks if the GC log directory exists, if not, create it
+if [ ! -d "$LOG_GC_DIR" ]; then
+    mkdir -p "$LOG_GC_DIR"
+    if [ $? -ne 0 ]; then
+        echo "Error creating log directory for GC. Make sure you have the proper permissions."
+        exit 1
+    fi
+else
+    echo "The log directory already exists."
+fi
+
 JAVA_OPTS=" ${JAVA_OPTS} -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=200m -Xmx${ranger_admin_max_heap_size} -Xms1g -Xloggc:${RANGER_ADMIN_LOG_DIR}/gc-worker.log -verbose:gc -XX:+PrintGCDetails"
 if [[ ${JAVA_OPTS} != *"-Duser.timezone"* ]] ;then  export JAVA_OPTS=" ${JAVA_OPTS} -Duser.timezone=UTC" ;fi
 
