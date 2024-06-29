1942172cf278e0142e87040637068b3c91064fe9
Ramesh Mani <rmani@apache.org>
2024-06-29 10:28:12 -0700

    RANGER-4835:RangerHiveAuthorizer audit enhancement for metadata operations like show table and databases

--- hive-agent/src/main/java/org/apache/ranger/authorization/hive/authorizer/RangerHiveAccessRequest.java
+++ hive-agent/src/main/java/org/apache/ranger/authorization/hive/authorizer/RangerHiveAccessRequest.java
@@ -25,9 +25,12 @@ import java.util.Set;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveAuthzSessionContext;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveOperationType;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveAuthzContext;
+import org.apache.ranger.audit.utils.StringUtil;
 import org.apache.ranger.plugin.policyengine.RangerAccessRequestImpl;
 import org.apache.ranger.plugin.policyengine.RangerPolicyEngine;
 import org.apache.ranger.plugin.util.RangerAccessRequestUtil;
+
+import static org.apache.ranger.authorization.hive.authorizer.RangerHiveAuditHandler.ACTION_TYPE_METADATA_OPERATION;
 
 public class RangerHiveAccessRequest extends RangerAccessRequestImpl {
 	private HiveAccessType accessType = HiveAccessType.NONE;
@@ -56,6 +59,15 @@ public class RangerHiveAccessRequest extends RangerAccessRequestImpl {
 			this.setRequestData(context.getCommandString());
 			this.setForwardedAddresses(context.getForwardedAddresses());
 			this.setRemoteIPAddress(context.getIpAddress());
+			String requestData = context.getCommandString();
+			if (StringUtil.isEmpty(requestData) && ACTION_TYPE_METADATA_OPERATION.equals(hiveOpTypeName)) {
+				String resourceType = resource.getObjectType().name();
+				if (resourceType.equalsIgnoreCase("DATABASE")) {
+					this.setRequestData("show databases");
+				} else if (resourceType.equalsIgnoreCase("TABLE")) {
+					this.setRequestData("show tables / views");
+				}
+			}
 		}
 
 		if(sessionContext != null) {
