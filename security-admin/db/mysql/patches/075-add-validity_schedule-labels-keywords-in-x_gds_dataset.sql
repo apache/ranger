89963fbefa753281060ef34327ef0a1305e93089
Radhika Kundam <radhika.kundam@gmail.com>
2024-11-08 12:04:46 -0800

    RANGER-4989: DB patch fix for add_validity_schedule_labels_keywords_in_x_gds_dataset in mysql
    Signed-off-by: Ramesh Mani <rmani@apache.org>

--- security-admin/db/mysql/patches/075-add-validity_schedule-labels-keywords-in-x_gds_dataset.sql
+++ security-admin/db/mysql/patches/075-add-validity_schedule-labels-keywords-in-x_gds_dataset.sql
@@ -13,7 +13,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-drop procedure if exists add_validity_schedule_labels_keywords_in_x_gds_dataset();
+drop procedure if exists add_validity_schedule_labels_keywords_in_x_gds_dataset;
 
 delimiter ;;
 create procedure add_validity_schedule_labels_keywords_in_x_gds_dataset() begin
