d71ad0cf0187d8bb6e6c3b3a9e1d87454e0b9113
Madhan Neethiraj <madhan@apache.org>
2024-12-05 14:09:26 -0800

    RANGER-5018: checkstyle complince updates - agents-audit

--- agents-audit/src/main/java/org/apache/ranger/audit/queue/AuditAsyncQueue.java
+++ agents-audit/src/main/java/org/apache/ranger/audit/queue/AuditAsyncQueue.java
@@ -19,186 +19,200 @@
 
 package org.apache.ranger.audit.queue;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.concurrent.LinkedBlockingQueue;
-
 import org.apache.ranger.audit.model.AuditEventBase;
 import org.apache.ranger.audit.provider.AuditHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * This is a non-blocking queue with no limit on capacity.
  */
 public class AuditAsyncQueue extends AuditQueue implements Runnable {
-	private static final Logger logger = LoggerFactory.getLogger(AuditAsyncQueue.class);
+    private static final Logger logger = LoggerFactory.getLogger(AuditAsyncQueue.class);
 
-	LinkedBlockingQueue<AuditEventBase> queue = new LinkedBlockingQueue<AuditEventBase>();
-	Thread consumerThread = null;
+    static final int    MAX_DRAIN    = 1000;
+    static final String DEFAULT_NAME = "async";
+    static       int    threadCount;
 
-	static final int MAX_DRAIN = 1000;
-	static int threadCount = 0;
-	static final String DEFAULT_NAME = "async";
+    LinkedBlockingQueue<AuditEventBase> queue          = new LinkedBlockingQueue<>();
+    Thread                              consumerThread;
 
-	public AuditAsyncQueue(AuditHandler consumer) {
-		super(consumer);
-		setName(DEFAULT_NAME);
-	}
+    public AuditAsyncQueue(AuditHandler consumer) {
+        super(consumer);
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see
-	 * org.apache.ranger.audit.provider.AuditProvider#log(org.apache.ranger.
-	 * audit.model.AuditEventBase)
-	 */
-	@Override
-	public boolean log(AuditEventBase event) {
-		logStatusIfRequired();
+        setName(DEFAULT_NAME);
+    }
 
-		addTotalCount(1);
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.apache.ranger.audit.provider.AuditProvider#log(org.apache.ranger.
+     * audit.model.AuditEventBase)
+     */
+    @Override
+    public boolean log(AuditEventBase event) {
+        logStatusIfRequired();
 
-		// Add to the queue and return ASAP
-		if (queue.size() >= getMaxQueueSize()) {
-			addFailedCount(1);
-			return false;
-		}
-		queue.add(event);
-		return true;
-	}
+        addTotalCount(1);
 
-	@Override
-	public boolean log(Collection<AuditEventBase> events) {
-		boolean ret = true;
-		for (AuditEventBase event : events) {
-			ret = log(event);
-			if (!ret) {
-				break;
-			}
-		}
-		return ret;
-	}
+        // Add to the queue and return ASAP
+        if (queue.size() >= getMaxQueueSize()) {
+            addFailedCount(1);
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see org.apache.ranger.audit.provider.AuditProvider#start()
-	 */
-	@Override
-	public void start() {
-		if (consumer != null) {
-			consumer.start();
-		} else {
-			logger.error("consumer is not set. Nothing will be sent to any consumer. name="
-					+ getName());
-		}
+            return false;
+        }
 
-		consumerThread = new Thread(this, this.getClass().getName()
-				+ (threadCount++));
-		consumerThread.setDaemon(true);
-		consumerThread.start();
-	}
+        queue.add(event);
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see org.apache.ranger.audit.provider.AuditProvider#stop()
-	 */
-	@Override
-	public void stop() {
-		logger.info("Stop called. name=" + getName());
-		setDrain(true);
-		try {
-			if (consumerThread != null) {
-				logger.info("Interrupting consumerThread. name=" + getName()
-						+ ", consumer="
-						+ (consumer == null ? null : consumer.getName()));
-				consumerThread.interrupt();
-			}
-		} catch (Throwable t) {
-			// ignore any exception
-		}
-		consumerThread = null;
-	}
+        return true;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.lang.Runnable#run()
-	 */
-	@Override
-	public void run() {
-		try {
-			//This is done to clear the MDC context to avoid issue with Ranger Auditing for Knox
-			MDC.clear();
-			runLogAudit();
-		} catch (Throwable t) {
-			logger.error("Exited thread abnormaly. queue=" + getName(), t);
-		}
-	}
+    @Override
+    public void logStatus() {
+        super.logStatus();
 
-	@Override
-	public void logStatus() {
-		super.logStatus();
+        if (isStatusLogEnabled()) {
+            logger.info("AuditAsyncQueue.log(name={}): totalCount={}, currentQueueLength={}", getName(), getTotalCount(), queue.size());
+        }
+    }
 
-		if (isStatusLogEnabled()) {
-			logger.info("AuditAsyncQueue.log(name={}): totalCount={}, currentQueueLength={}", getName(), getTotalCount(), queue.size());
-		}
-	}
+    @Override
+    public boolean log(Collection<AuditEventBase> events) {
+        boolean ret = true;
 
-	public int size() { return queue.size(); }
+        for (AuditEventBase event : events) {
+            ret = log(event);
 
-	public void runLogAudit() {
-		while (true) {
-			try {
-				AuditEventBase event = null;
-				if (!isDrain()) {
-					// For Transfer queue take() is blocking
-					event = queue.take();
-				} else {
-					// For Transfer queue poll() is non blocking
-					event = queue.poll();
-				}
-				if (event != null) {
-					Collection<AuditEventBase> eventList = new ArrayList<AuditEventBase>();
-					eventList.add(event);
-					queue.drainTo(eventList, MAX_DRAIN - 1);
-					consumer.log(eventList);
+            if (!ret) {
+                break;
+            }
+        }
 
-					logStatusIfRequired();
-				}
-			} catch (InterruptedException e) {
-				logger.info("Caught exception in consumer thread. Shutdown might be in progress");
-			} catch (Throwable t) {
-				logger.error("Caught error during processing request.", t);
-			}
-			if (isDrain()) {
-				if (queue.isEmpty()) {
-					break;
-				}
-				if (isDrainMaxTimeElapsed()) {
-					logger.warn("Exiting polling loop because max time allowed reached. name="
-							+ getName()
-							+ ", waited for "
-							+ (stopTime - System.currentTimeMillis()) + " ms");
-				}
-			}
-		}
-		logger.info("Exiting polling loop. name=" + getName());
+        return ret;
+    }
 
-		try {
-			// Call stop on the consumer
-			logger.info("Calling to stop consumer. name=" + getName()
-					+ ", consumer.name=" + consumer.getName());
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.ranger.audit.provider.AuditProvider#start()
+     */
+    @Override
+    public void start() {
+        if (consumer != null) {
+            consumer.start();
+        } else {
+            logger.error("consumer is not set. Nothing will be sent to any consumer. name={}", getName());
+        }
 
-			// Call stop on the consumer
-			consumer.stop();
-		} catch (Throwable t) {
-			logger.error("Error while calling stop on consumer.", t);
-		}
-		logger.info("Exiting consumerThread.run() method. name=" + getName());
-	}
+        consumerThread = new Thread(this, this.getClass().getName() + (threadCount++));
 
+        consumerThread.setDaemon(true);
+        consumerThread.start();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.ranger.audit.provider.AuditProvider#stop()
+     */
+    @Override
+    public void stop() {
+        logger.info("Stop called. name={}", getName());
+
+        setDrain(true);
+
+        try {
+            if (consumerThread != null) {
+                logger.info("Interrupting consumerThread. name={}, consumer={}", getName(), (consumer == null ? null : consumer.getName()));
+
+                consumerThread.interrupt();
+            }
+        } catch (Throwable t) {
+            // ignore any exception
+        }
+
+        consumerThread = null;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.lang.Runnable#run()
+     */
+    @Override
+    public void run() {
+        try {
+            //This is done to clear the MDC context to avoid issue with Ranger Auditing for Knox
+            MDC.clear();
+            runLogAudit();
+        } catch (Throwable t) {
+            logger.error("Exited thread abnormaly. queue={}", getName(), t);
+        }
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public void runLogAudit() {
+        while (true) {
+            try {
+                final AuditEventBase event;
+
+                if (!isDrain()) {
+                    // For Transfer queue take() is blocking
+                    event = queue.take();
+                } else {
+                    // For Transfer queue poll() is non blocking
+                    event = queue.poll();
+                }
+
+                if (event != null) {
+                    Collection<AuditEventBase> eventList = new ArrayList<>();
+
+                    eventList.add(event);
+
+                    queue.drainTo(eventList, MAX_DRAIN - 1);
+
+                    consumer.log(eventList);
+
+                    logStatusIfRequired();
+                }
+            } catch (InterruptedException e) {
+                logger.info("Caught exception in consumer thread. Shutdown might be in progress");
+            } catch (Throwable t) {
+                logger.error("Caught error during processing request.", t);
+            }
+
+            if (isDrain()) {
+                if (queue.isEmpty()) {
+                    break;
+                }
+
+                if (isDrainMaxTimeElapsed()) {
+                    logger.warn("Exiting polling loop because max time allowed reached. name={}, waited for {} ms", getName(), (stopTime - System.currentTimeMillis()));
+                }
+            }
+        }
+
+        logger.info("Exiting polling loop. name={}", getName());
+
+        try {
+            // Call stop on the consumer
+            logger.info("Calling to stop consumer. name={}, consumer.name={}", getName(), consumer.getName());
+
+            // Call stop on the consumer
+            consumer.stop();
+        } catch (Throwable t) {
+            logger.error("Error while calling stop on consumer.", t);
+        }
+
+        logger.info("Exiting consumerThread.run() method. name={}", getName());
+    }
 }
