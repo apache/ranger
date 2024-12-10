55d759fda6fea14b8dc99abc9eef2f659049039c
Abhishek Kumar <abhishekkumar100031@gmail.com>
2024-12-09 20:13:27 -0800

    RANGER-5054: checkstyle compliance updates - ranger-authn (#432)

--- ranger-authn/src/main/java/org/apache/ranger/authz/handler/jwt/RangerDefaultJwtAuthHandler.java
+++ ranger-authn/src/main/java/org/apache/ranger/authz/handler/jwt/RangerDefaultJwtAuthHandler.java
@@ -18,29 +18,52 @@
  */
 package org.apache.ranger.authz.handler.jwt;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.security.authentication.server.AuthenticationToken;
-import org.apache.ranger.authz.handler.RangerAuth;
-
 import com.nimbusds.jose.proc.JWSKeySelector;
 import com.nimbusds.jose.proc.SecurityContext;
 import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
 import com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier;
 import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import com.nimbusds.jwt.proc.JWTClaimsSetVerifier;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.security.authentication.server.AuthenticationToken;
+import org.apache.ranger.authz.handler.RangerAuth;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Default implementation of Ranger JWT authentication
- *
  */
 public class RangerDefaultJwtAuthHandler extends RangerJwtAuthHandler {
-
     protected static final String AUTHORIZATION_HEADER = "Authorization";
     protected static final String DO_AS_PARAMETER      = "doAs";
+
+    public static boolean canAuthenticateRequest(final ServletRequest request) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String             jwtAuthHeaderStr   = getJwtAuthHeader(httpServletRequest);
+        String             jwtCookieStr       = StringUtils.isBlank(jwtAuthHeaderStr) ? getJwtCookie(httpServletRequest) : null;
+
+        return shouldProceedAuth(jwtAuthHeaderStr, jwtCookieStr);
+    }
+
+    public static String getJwtAuthHeader(final HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getHeader(AUTHORIZATION_HEADER);
+    }
+
+    public static String getJwtCookie(final HttpServletRequest httpServletRequest) {
+        String   jwtCookieStr = null;
+        Cookie[] cookies      = httpServletRequest.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookieName.equals(cookie.getName())) {
+                    jwtCookieStr = cookie.getName() + "=" + cookie.getValue();
+                    break;
+                }
+            }
+        }
+        return jwtCookieStr;
+    }
 
     @Override
     public ConfigurableJWTProcessor<SecurityContext> getJwtProcessor(JWSKeySelector<SecurityContext> keySelector) {
@@ -63,35 +86,9 @@ public class RangerDefaultJwtAuthHandler extends RangerJwtAuthHandler {
         AuthenticationToken authenticationToken = authenticate(jwtAuthHeaderStr, jwtCookieStr, doAsUser);
 
         if (authenticationToken != null) {
-            rangerAuth = new RangerAuth(authenticationToken, RangerAuth.AUTH_TYPE.JWT_JWKS);
+            rangerAuth = new RangerAuth(authenticationToken, RangerAuth.AuthType.JWT_JWKS);
         }
 
         return rangerAuth;
-    }
-
-    public static boolean canAuthenticateRequest(final ServletRequest request) {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        String     jwtAuthHeaderStr = getJwtAuthHeader(httpServletRequest);
-        String     jwtCookieStr     = StringUtils.isBlank(jwtAuthHeaderStr) ? getJwtCookie(httpServletRequest) : null;
-
-        return shouldProceedAuth(jwtAuthHeaderStr, jwtCookieStr);
-    }
-
-    public static String getJwtAuthHeader(final HttpServletRequest httpServletRequest) {
-        return httpServletRequest.getHeader(AUTHORIZATION_HEADER);
-    }
-
-    public static String getJwtCookie(final HttpServletRequest httpServletRequest) {
-        String jwtCookieStr = null;
-        Cookie[] cookies = httpServletRequest.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookieName.equals(cookie.getName())) {
-                    jwtCookieStr = cookie.getName() + "=" + cookie.getValue();
-                    break;
-                }
-            }
-        }
-        return jwtCookieStr;
     }
 }
