dac09b84ef625e79864dd4f9c39600f1bc087c4d
Abhishek Kumar <abhi@apache.org>
2024-12-11 19:44:59 -0800

    RANGER-5043: checkstyle compliance updates - plugin-nestedstructure module

--- plugin-nestedstructure/src/main/java/org/apache/ranger/authorization/nestedstructure/authorizer/NestedStructureResource.java
+++ plugin-nestedstructure/src/main/java/org/apache/ranger/authorization/nestedstructure/authorizer/NestedStructureResource.java
@@ -1,20 +1,20 @@
 /**
-* Copyright 2022 Comcast Cable Communications Management, LLC
-*
-* Licensed under the Apache License, Version 2.0 (the ""License"");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an ""AS IS"" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or   implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* SPDX-License-Identifier: Apache-2.0
-*/
+ * Copyright 2022 Comcast Cable Communications Management, LLC
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the ""License"");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an ""AS IS"" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or   implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 package org.apache.ranger.authorization.nestedstructure.authorizer;
 
@@ -23,10 +23,10 @@ import org.apache.ranger.plugin.policyengine.RangerAccessResourceImpl;
 import java.util.Optional;
 
 public class NestedStructureResource extends RangerAccessResourceImpl {
-  public static final String KEY_SCHEMA = "schema";
-  public static final String KEY_FIELD  = "field";
+    public static final String KEY_SCHEMA = "schema";
+    public static final String KEY_FIELD  = "field";
 
-  public NestedStructureResource(Optional<String> schema, Optional<String> field) {
+    public NestedStructureResource(Optional<String> schema, Optional<String> field) {
         if (schema.isPresent()) {
             setValue(KEY_SCHEMA, schema.get());
         }
@@ -42,5 +42,3 @@ public class NestedStructureResource extends RangerAccessResourceImpl {
         }
     }
 }
-
-
--- plugin-nestedstructure/src/test/java/org/apache/ranger/authorization/nestedstructure/authorizer/ExampleClient.java
+++ plugin-nestedstructure/src/test/java/org/apache/ranger/authorization/nestedstructure/authorizer/ExampleClient.java
@@ -1,32 +1,34 @@
 /**
-* Copyright 2022 Comcast Cable Communications Management, LLC
-*
-* Licensed under the Apache License, Version 2.0 (the ""License"");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an ""AS IS"" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or   implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* SPDX-License-Identifier: Apache-2.0
-*/
+ * Copyright 2022 Comcast Cable Communications Management, LLC
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the ""License"");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an ""AS IS"" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or   implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 package org.apache.ranger.authorization.nestedstructure.authorizer;
-
 
 import java.util.Set;
 
 public class ExampleClient {
-    public static void main( String[] args) {
+    private ExampleClient(){
+    }
+
+    public static void main(String[] args) {
         String      schema     = "json_object.foo.v1";
         String      userName   = "someuser";
         Set<String> userGroups = null;
-        String      jsonString = "{\n" +
+        String jsonString = "{\n" +
                 "    \"foo\": \"12345678\",\n" +
                 "    \"address\": {\n" +
                 "      \"city\": \"philadelphia\" \n" +
@@ -36,11 +38,11 @@ public class ExampleClient {
 
         AccessResult result = NestedStructureAuthorizer.getInstance().authorize(schema, userName, userGroups, jsonString, NestedStructureAccessType.READ);
 
-        System.out.println("has errors: "+ result.hasErrors());
-        System.out.println("hasAccess: "+ result.hasAccess());
-        System.out.println("authorizedJson: "+ result.getJson());
+        System.out.println("has errors: " + result.hasErrors());
+        System.out.println("hasAccess: " + result.hasAccess());
+        System.out.println("authorizedJson: " + result.getJson());
 
-        result.getErrors().stream().forEach(e-> e.printStackTrace());
+        result.getErrors().stream().forEach(e -> e.printStackTrace());
 
         System.out.println("done");
     }
