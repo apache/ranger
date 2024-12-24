49710d51c94e130025bf53d77984e8812539c303
Madhan Neethiraj <madhan@apache.org>
2024-12-23 16:22:43 -0800

    Update KMSServerJSONUtils.java

--- kms/src/main/java/org/apache/hadoop/crypto/key/kms/server/KMSServerJSONUtils.java
+++ kms/src/main/java/org/apache/hadoop/crypto/key/kms/server/KMSServerJSONUtils.java
@@ -38,17 +38,20 @@ public class KMSServerJSONUtils {
     @SuppressWarnings("unchecked")
     public static List toJSON(List<KeyProvider.KeyVersion> keyVersions) {
         List json = new ArrayList();
+
         if (keyVersions != null) {
             for (KeyProvider.KeyVersion version : keyVersions) {
                 json.add(KMSUtil.toJSON(version));
             }
         }
+
         return json;
     }
 
     @SuppressWarnings("unchecked")
     public static Map toJSON(String keyName, KeyProvider.Metadata meta) {
         Map json = new LinkedHashMap();
+
         if (meta != null) {
             json.put(KMSRESTConstants.NAME_FIELD, keyName);
             json.put(KMSRESTConstants.CIPHER_FIELD, meta.getCipher());
@@ -58,15 +61,18 @@ public class KMSServerJSONUtils {
             json.put(KMSRESTConstants.CREATED_FIELD, meta.getCreated().getTime());
             json.put(KMSRESTConstants.VERSIONS_FIELD, (long) meta.getVersions());
         }
+
         return json;
     }
 
     @SuppressWarnings("unchecked")
     public static List toJSON(String[] keyNames, KeyProvider.Metadata[] metas) {
         List json = new ArrayList();
+
         for (int i = 0; i < keyNames.length; i++) {
             json.add(toJSON(keyNames[i], metas[i]));
         }
+
         return json;
     }
 }
