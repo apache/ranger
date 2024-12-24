94348cc3c460fa2d8df08dcb7ca694159724a35b
Madhan Neethiraj <madhan@apache.org>
2024-12-23 17:35:16 -0800

    Update RangerGoogleCloudHSMProvider.java

--- kms/src/main/java/org/apache/hadoop/crypto/key/RangerGoogleCloudHSMProvider.java
+++ kms/src/main/java/org/apache/hadoop/crypto/key/RangerGoogleCloudHSMProvider.java
@@ -40,21 +40,23 @@ import java.security.Key;
 import java.util.Map;
 
 public class RangerGoogleCloudHSMProvider implements RangerKMSMKI {
-    private static final   Logger logger = LoggerFactory.getLogger(RangerGoogleCloudHSMProvider.class);
+    private static final Logger logger = LoggerFactory.getLogger(RangerGoogleCloudHSMProvider.class);
+
     protected static final String GCP_KEYRING_ID        = "ranger.kms.gcp.keyring.id";
     protected static final String GCP_CRED_JSON_FILE    = "ranger.kms.gcp.cred.file";
     protected static final String GCP_PROJECT_ID        = "ranger.kms.gcp.project.id";
     protected static final String GCP_LOCATION_ID       = "ranger.kms.gcp.location.id";
     protected static final String GCP_MASTER_KEY_NAME   = "ranger.kms.gcp.masterkey.name";
-    private static final   String GCP_CRED_ENV_VARIABLE = "GOOGLE_APPLICATION_CREDENTIALS";
+    private   static final String GCP_CRED_ENV_VARIABLE = "GOOGLE_APPLICATION_CREDENTIALS";
 
-    private String gcpKeyRingId;
-    private String gcpAppCredFile;
-    private String gcpProjectId;
-    private String gcpLocationId;
-    private String gcpMasterKeyName;
+    private final String gcpKeyRingId;
+    private final String gcpAppCredFile;
+    private final String gcpProjectId;
+    private final String gcpLocationId;
+    private final String gcpMasterKeyName;
+
     private KeyManagementServiceClient client;
-    private KeyRingName keyRingName;
+    private KeyRingName                keyRingName;
 
     public RangerGoogleCloudHSMProvider(Configuration conf) throws Exception {
         this.gcpKeyRingId     = conf.get(GCP_KEYRING_ID);
@@ -82,7 +84,7 @@ public class RangerGoogleCloudHSMProvider implements RangerKMSMKI {
             createdKey = client.createCryptoKey(this.keyRingName, this.gcpMasterKeyName, key);
         } catch (Exception e) {
             if (e instanceof AlreadyExistsException) {
-                logger.info("MasterKey with the name '" + this.gcpMasterKeyName + "' already exist.");
+                logger.info("MasterKey with the name '{}' already exist.", this.gcpMasterKeyName);
                 return true;
             } else {
                 throw new RuntimeCryptoException("Failed to create master key with name '" + this.gcpMasterKeyName + "', Error - " + e.getMessage());
@@ -90,10 +92,12 @@ public class RangerGoogleCloudHSMProvider implements RangerKMSMKI {
         }
 
         if (createdKey == null) {
-            logger.info("Failed to create master key : " + this.gcpMasterKeyName);
+            logger.info("Failed to create master key : {}", this.gcpMasterKeyName);
             return false;
         }
-        logger.info("Master Key Created Successfully On Google Cloud HSM : " + this.gcpMasterKeyName);
+
+        logger.info("Master Key Created Successfully On Google Cloud HSM : {}", this.gcpMasterKeyName);
+
         return true;
     }
 
@@ -106,45 +110,57 @@ public class RangerGoogleCloudHSMProvider implements RangerKMSMKI {
     @Override
     public byte[] decryptZoneKey(byte[] encryptedByte) throws Exception {
         CryptoKeyName keyName = CryptoKeyName.of(this.gcpProjectId, this.gcpLocationId, this.gcpKeyRingId, this.gcpMasterKeyName);
+
         logger.debug("==> GCP decryptZoneKey() : CryptoKeyName - { {} }", keyName);
 
         DecryptResponse response = client.decrypt(keyName, ByteString.copyFrom(encryptedByte));
+
         if (response == null) {
             throw new RuntimeCryptoException("Got null response for decrypt zone key operation!");
         } else if (response.getPlaintext() == null || StringUtils.isEmpty(response.getPlaintext().toString())) {
             throw new RuntimeCryptoException("Error - Received null or empty decrypted zone key : " + response.getPlaintext());
         }
+
         logger.debug("<== GCP decryptZoneKey() : DecryptResponse - { {} }", response);
+
         return response.getPlaintext().toByteArray();
     }
 
     @Override
     public byte[] encryptZoneKey(Key zoneKey) throws Exception {
         logger.debug("==> GCP encryptZoneKey()");
-        byte[]  primaryEncodedZoneKey = zoneKey.getEncoded(); // Data to encrypt i.e a zoneKey
-        CryptoKeyName keyName = CryptoKeyName.of(this.gcpProjectId, this.gcpLocationId, this.gcpKeyRingId, this.gcpMasterKeyName);
 
-        EncryptResponse encryptResponse = this.client.encrypt(keyName, ByteString.copyFrom(primaryEncodedZoneKey));
+        byte[]          primaryEncodedZoneKey = zoneKey.getEncoded(); // Data to encrypt i.e a zoneKey
+        CryptoKeyName   keyName               = CryptoKeyName.of(this.gcpProjectId, this.gcpLocationId, this.gcpKeyRingId, this.gcpMasterKeyName);
+        EncryptResponse encryptResponse       = this.client.encrypt(keyName, ByteString.copyFrom(primaryEncodedZoneKey));
+
         if (encryptResponse == null) {
             throw new RuntimeCryptoException("Got null response for encrypt zone key operation, Please reverify/check configs!");
         }
+
         logger.debug("<== GCP encryptZoneKey() : EncryptResponse - { {} }", encryptResponse);
+
         return encryptResponse.getCiphertext().toByteArray();
     }
 
     @Override
     public void onInitialization() throws Exception {
         this.validateGcpProps();
+
         logger.debug("==> onInitialization() : {gcpProjectId - {} , gcpLocationId - {} , gcpKeyRingId - {} , gcpAppCredFile Path - {} }",
                     this.gcpProjectId, this.gcpLocationId, this.gcpKeyRingId, this.gcpAppCredFile);
-        String errorMessage = null;
+
+        String  errorMessage    = null;
+        KeyRing keyRingResponse = null;
+
         client = getKeyClient(this.gcpAppCredFile);
 
-        KeyRing keyRingResponse = null;
         if (client != null) {
             this.keyRingName = KeyRingName.of(this.gcpProjectId, this.gcpLocationId, this.gcpKeyRingId);
+
             if (this.keyRingName != null) {
                 keyRingResponse = this.client.getKeyRing(this.keyRingName.toString());
+
                 if (keyRingResponse == null) {
                     errorMessage = "Unable to get Key Ring response for Project : " + this.gcpProjectId + " and Location : " + this.gcpLocationId;
                 } else if (keyRingResponse != null && !keyRingResponse.getName().endsWith(this.gcpKeyRingId)) {
@@ -156,7 +172,9 @@ public class RangerGoogleCloudHSMProvider implements RangerKMSMKI {
         } else {
             errorMessage = "Unable to create client object for Google Cloud HSM. Please check the Key HSM Log file OR Verify Google App Credential JSON file.";
         }
+
         logger.debug("<== onInitialization() : {this.keyRingName - {}, keyRingResponse - {} }", this.keyRingName, keyRingResponse);
+
         if (!StringUtils.isEmpty(errorMessage)) {
             throw new RuntimeCryptoException(errorMessage);
         }
@@ -181,21 +199,26 @@ public class RangerGoogleCloudHSMProvider implements RangerKMSMKI {
             if (StringUtils.isEmpty(System.getenv(GCP_CRED_ENV_VARIABLE))) {
                 updateEnv(GCP_CRED_ENV_VARIABLE, credentialFileName);
             }
+
             KeyManagementServiceClient client = KeyManagementServiceClient.create();
+
             return client;
         } catch (Exception ex) {
             logger.error("Unable to create Google Cloud KMS Client, Error : ", ex);
         }
+
         return null;
     }
 
     @SuppressWarnings("unchecked")
     private static void updateEnv(String name, String val) throws ReflectiveOperationException {
         Map<String, String> env   = System.getenv();
-        Field field = env.getClass().getDeclaredField("m");
+        Field               field = env.getClass().getDeclaredField("m");
+
         field.setAccessible(true);
 
         Map<String, String> writeAbleEnvMap = (Map<String, String>) field.get(env);
+
         writeAbleEnvMap.put(name, val);
     }
 }
