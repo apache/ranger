ee6e2badb202a6ad2d3c8cfb20877ac99d53fb9d
Madhan Neethiraj <madhan@apache.org>
2024-12-23 17:29:36 -0800

    Update RangerAzureKeyVaultKeyGenerator.java

--- kms/src/main/java/org/apache/hadoop/crypto/key/RangerAzureKeyVaultKeyGenerator.java
+++ kms/src/main/java/org/apache/hadoop/crypto/key/RangerAzureKeyVaultKeyGenerator.java
@@ -35,21 +35,23 @@ import java.security.Key;
 
 public class RangerAzureKeyVaultKeyGenerator implements RangerKMSMKI {
     static final Logger logger = LoggerFactory.getLogger(RangerAzureKeyVaultKeyGenerator.class);
-    static final String         AZURE_KEYVAULT_URL                  = "ranger.kms.azurekeyvault.url";
-    static final String         AZURE_MASTER_KEY_ALIAS              = "ranger.kms.azure.masterkey.name";
-    static final String         AZURE_MASTER_KEY_TYPE               = "ranger.kms.azure.masterkey.type";
-    static final String         ZONE_KEY_ENCRYPTION_ALGO            = "ranger.kms.azure.zonekey.encryption.algorithm";
-    static final String         AZURE_KEYVAULT_SSL_ENABLED          = "ranger.kms.azure.keyvault.ssl.enabled";
-    static final String         AZURE_CLIENT_ID                     = "ranger.kms.azure.client.id";
-    static final String         AZURE_CLIENT_SECRET                 = "ranger.kms.azure.client.secret";
-    static final String         AZURE_KEYVAULT_CERTIFICATE_PATH     = "ranger.kms.azure.keyvault.certificate.path";
-    static final String         AZURE_KEYVAULT_CERTIFICATE_PASSWORD = "ranger.kms.azure.keyvault.certificate.password";
-    private      String         keyVaultURL;
-    private      String         azureMasterKey;
-    private      String         azureMasterKeyType;
-    private      String         zoneKeyEncryptionAlgo;
-    private      KeyVaultClient keyVaultClient;
-    private      KeyBundle      masterKeyBundle;
+
+    static final String AZURE_KEYVAULT_URL                  = "ranger.kms.azurekeyvault.url";
+    static final String AZURE_MASTER_KEY_ALIAS              = "ranger.kms.azure.masterkey.name";
+    static final String AZURE_MASTER_KEY_TYPE               = "ranger.kms.azure.masterkey.type";
+    static final String ZONE_KEY_ENCRYPTION_ALGO            = "ranger.kms.azure.zonekey.encryption.algorithm";
+    static final String AZURE_KEYVAULT_SSL_ENABLED          = "ranger.kms.azure.keyvault.ssl.enabled";
+    static final String AZURE_CLIENT_ID                     = "ranger.kms.azure.client.id";
+    static final String AZURE_CLIENT_SECRET                 = "ranger.kms.azure.client.secret";
+    static final String AZURE_KEYVAULT_CERTIFICATE_PATH     = "ranger.kms.azure.keyvault.certificate.path";
+    static final String AZURE_KEYVAULT_CERTIFICATE_PASSWORD = "ranger.kms.azure.keyvault.certificate.password";
+
+    private String         keyVaultURL;
+    private String         azureMasterKey;
+    private String         azureMasterKeyType;
+    private String         zoneKeyEncryptionAlgo;
+    private KeyVaultClient keyVaultClient;
+    private KeyBundle      masterKeyBundle;
 
     public RangerAzureKeyVaultKeyGenerator(Configuration conf, KeyVaultClient kvClient) {
         this.keyVaultURL           = conf.get(AZURE_KEYVAULT_URL);
@@ -65,30 +67,37 @@ public class RangerAzureKeyVaultKeyGenerator implements RangerKMSMKI {
 
     public static KeyVaultClient createKeyVaultClient(Configuration conf) throws Exception {
         AzureKeyVaultClientAuthenticator azureKVClientAuthenticator;
+
         String azureClientId = conf.get(AZURE_CLIENT_ID);
+
         if (StringUtils.isEmpty(azureClientId)) {
             throw new Exception("Azure Key Vault is enabled and client id is not configured");
         }
-        String azureClientSecret = conf.get(AZURE_CLIENT_SECRET);
+
+        String         azureClientSecret = conf.get(AZURE_CLIENT_SECRET);
         KeyVaultClient kvClient;
-        if (conf != null && StringUtils.isNotEmpty(conf.get(AZURE_KEYVAULT_SSL_ENABLED))
-                && conf.get(AZURE_KEYVAULT_SSL_ENABLED).equalsIgnoreCase("false")) {
+
+        if (conf != null && StringUtils.isNotEmpty(conf.get(AZURE_KEYVAULT_SSL_ENABLED)) && conf.get(AZURE_KEYVAULT_SSL_ENABLED).equalsIgnoreCase("false")) {
             try {
                 if (StringUtils.isEmpty(azureClientSecret)) {
                     throw new Exception("Azure Key Vault is enabled in non SSL mode and client password/secret is not configured");
                 }
+
                 azureKVClientAuthenticator = new AzureKeyVaultClientAuthenticator(azureClientId, azureClientSecret);
-                kvClient = new KeyVaultClient(azureKVClientAuthenticator);
+                kvClient                   = new KeyVaultClient(azureKVClientAuthenticator);
             } catch (Exception ex) {
                 throw new Exception("Error while getting key vault client object with client id and client secret : " + ex);
             }
         } else {
             try {
                 azureKVClientAuthenticator = new AzureKeyVaultClientAuthenticator(azureClientId);
+
                 String keyVaultCertPath = conf.get(AZURE_KEYVAULT_CERTIFICATE_PATH);
+
                 if (StringUtils.isEmpty(keyVaultCertPath)) {
                     throw new Exception("Azure Key Vault is enabled in SSL mode. Please provide certificate path for authentication.");
                 }
+
                 String keyVaultCertPassword = conf.get(AZURE_KEYVAULT_CERTIFICATE_PASSWORD);
 
                 kvClient = !StringUtils.isEmpty(keyVaultCertPassword) ? azureKVClientAuthenticator.getAuthentication(keyVaultCertPath, keyVaultCertPassword)
@@ -97,6 +106,7 @@ public class RangerAzureKeyVaultKeyGenerator implements RangerKMSMKI {
                 throw new Exception("Error while getting key vault client object with client id and certificate. Error :  : " + ex);
             }
         }
+
         return kvClient;
     }
 
@@ -105,15 +115,17 @@ public class RangerAzureKeyVaultKeyGenerator implements RangerKMSMKI {
         if (keyVaultClient == null) {
             throw new Exception("Key Vault Client is null. Please check the azure related configuration.");
         }
+
         try {
             masterKeyBundle = keyVaultClient.getKey(keyVaultURL, azureMasterKey);
         } catch (Exception ex) {
-            throw new Exception("Error while getting existing master key from Azure.  Master Key Name : "
-                            + azureMasterKey + " . Key Vault URL : " + keyVaultURL + " . Error : " + ex.getMessage());
+            throw new Exception("Error while getting existing master key from Azure.  Master Key Name : " + azureMasterKey + " . Key Vault URL : " + keyVaultURL + " . Error : " + ex.getMessage());
         }
+
         if (masterKeyBundle == null) {
             try {
                 JsonWebKeyType keyType;
+
                 switch (azureMasterKeyType) {
                     case "RSA":
                         keyType = JsonWebKeyType.RSA;
@@ -139,17 +151,18 @@ public class RangerAzureKeyVaultKeyGenerator implements RangerKMSMKI {
                         keyType = JsonWebKeyType.RSA;
                 }
 
-                Attributes masterKeyattribute = new KeyAttributes().withEnabled(true).withNotBefore(new DateTime());
+                Attributes       masterKeyattribute = new KeyAttributes().withEnabled(true).withNotBefore(new DateTime());
+                CreateKeyRequest createKeyRequest   = new CreateKeyRequest.Builder(keyVaultURL, azureMasterKey, keyType).withAttributes(masterKeyattribute).build();
 
-                CreateKeyRequest createKeyRequest = new CreateKeyRequest.Builder(keyVaultURL, azureMasterKey, keyType)
-                        .withAttributes(masterKeyattribute).build();
                 masterKeyBundle = keyVaultClient.createKeyAsync(createKeyRequest, null).get();
+
                 return true;
             } catch (Exception ex) {
                 throw new Exception("Error while creating master key  : " + ex.getMessage());
             }
         } else {
             logger.info("Azure Master key exist with name :" + azureMasterKey + " with key identifier " + masterKeyBundle.key().kid());
+
             return true;
         }
     }
@@ -166,15 +179,19 @@ public class RangerAzureKeyVaultKeyGenerator implements RangerKMSMKI {
     @Override
     public byte[] decryptZoneKey(byte[] encryptedByte) throws Exception {
         JsonWebKeyEncryptionAlgorithm keyEncryptionAlgo = getZoneKeyEncryptionAlgo();
+
         if (masterKeyBundle == null) {
             masterKeyBundle = keyVaultClient.getKey(keyVaultURL, azureMasterKey);
         }
+
         KeyOperationResult decryptResult = null;
+
         try {
             decryptResult = keyVaultClient.decryptAsync(masterKeyBundle.key().kid(), keyEncryptionAlgo, encryptedByte, null).get();
         } catch (Exception e) {
             throw new Exception("Error while decrypting zone key." + e);
         }
+
         return decryptResult.result();
     }
 
@@ -191,11 +208,13 @@ public class RangerAzureKeyVaultKeyGenerator implements RangerKMSMKI {
         } catch (Exception e) {
             throw new Exception("Error while encrypting zone key." + e);
         }
+
         return encryptResult.result();
     }
 
     private JsonWebKeyEncryptionAlgorithm getZoneKeyEncryptionAlgo() {
         JsonWebKeyEncryptionAlgorithm keyEncryptionAlgo;
+
         switch (zoneKeyEncryptionAlgo) {
             case "RSA_OAEP":
                 keyEncryptionAlgo = JsonWebKeyEncryptionAlgorithm.RSA_OAEP;
@@ -212,6 +231,7 @@ public class RangerAzureKeyVaultKeyGenerator implements RangerKMSMKI {
             default:
                 keyEncryptionAlgo = JsonWebKeyEncryptionAlgorithm.RSA_OAEP;
         }
+
         return keyEncryptionAlgo;
     }
 }
