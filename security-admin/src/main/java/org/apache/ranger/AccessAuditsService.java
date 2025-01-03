5e2b185aa4c3f03bb930ec979b6c588f206c8e84
Pradeep AgrawaL <pradeep@apache.org>
2025-01-03 21:09:16 +0530

    RANGER-5061: checkstyle compliance updates - security-admin module - AccessAuditsService class

--- security-admin/src/main/java/org/apache/ranger/AccessAuditsService.java
+++ security-admin/src/main/java/org/apache/ranger/AccessAuditsService.java
@@ -30,83 +30,61 @@ import org.apache.ranger.db.RangerDaoManager;
 import org.apache.ranger.plugin.store.EmbeddedServiceDefsUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public class AccessAuditsService {
-    protected List<SortField> sortFields = new ArrayList<SortField>();
+    protected List<SortField>   sortFields = new ArrayList<SortField>();
     protected List<SearchField> searchFields;
+
     @Autowired
-    protected
-    RESTErrorUtil restErrorUtil;
+    protected RESTErrorUtil    restErrorUtil;
+
     @Autowired
-    protected
-    RangerDaoManager daoManager;
+    protected RangerDaoManager daoManager;
 
     public AccessAuditsService() {
         searchFields = new ArrayList<SearchField>();
-        searchFields.add(new SearchField("id", "id",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("accessType", "access",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("aclEnforcer", "enforcer",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("agentId", "agent",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("repoName", "repo",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("sessionId", "sess",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("requestUser", "reqUser",
-                SearchField.DATA_TYPE.STR_LIST, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("excludeUser", "exlUser",
-                SearchField.DATA_TYPE.STR_LIST, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("requestData", "reqData", SearchField.DATA_TYPE.STRING,
-                SearchField.SEARCH_TYPE.PARTIAL));
-        searchFields.add(new SearchField("resourcePath", "resource", SearchField.DATA_TYPE.STRING,
-                SearchField.SEARCH_TYPE.PARTIAL));
-        searchFields.add(new SearchField("clientIP", "cliIP",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("id", "id", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("accessType", "access", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("aclEnforcer", "enforcer", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("agentId", "agent", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("repoName", "repo", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("sessionId", "sess", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("requestUser", "reqUser", SearchField.DATA_TYPE.STR_LIST, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("excludeUser", "exlUser", SearchField.DATA_TYPE.STR_LIST, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("requestData", "reqData", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.PARTIAL));
+        searchFields.add(new SearchField("resourcePath", "resource", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.PARTIAL));
+        searchFields.add(new SearchField("clientIP", "cliIP", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
 
-        searchFields.add(new SearchField("auditType", "logType",
-                SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("accessResult", "result",
-                SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("auditType", "logType", SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("accessResult", "result", SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
         // searchFields.add(new SearchField("assetId", "obj.assetId",
         // SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("policyId", "policy",
-                SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("repoType", "repoType",
-                SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("policyId", "policy", SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("repoType", "repoType", SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
         /* Note; search fields starting with '-' denotes exclude conditions,
          * it should be handled manually if audit destination does not support the same.
          * solr support this way while cloudwatch does not.
          */
-        searchFields.add(new SearchField("-repoType", "-repoType",
-                SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("-requestUser", "-reqUser",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("resourceType", "resType",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("reason", "reason",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("action", "action",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("-repoType", "-repoType", SearchField.DATA_TYPE.INTEGER, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("-requestUser", "-reqUser", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("resourceType", "resType", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("reason", "reason", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("action", "action", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
 
-        searchFields.add(new SearchField("startDate", "evtTime",
-                SearchField.DATA_TYPE.DATE, SearchField.SEARCH_TYPE.GREATER_EQUAL_THAN));
-        searchFields.add(new SearchField("endDate", "evtTime", SearchField.DATA_TYPE.DATE,
-                SearchField.SEARCH_TYPE.LESS_EQUAL_THAN));
+        searchFields.add(new SearchField("startDate", "evtTime", SearchField.DATA_TYPE.DATE, SearchField.SEARCH_TYPE.GREATER_EQUAL_THAN));
+        searchFields.add(new SearchField("endDate", "evtTime", SearchField.DATA_TYPE.DATE, SearchField.SEARCH_TYPE.LESS_EQUAL_THAN));
 
         searchFields.add(new SearchField("tags", "tags", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.PARTIAL));
-        searchFields.add(new SearchField("cluster", "cluster",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("zoneName", "zoneName",
-                SearchField.DATA_TYPE.STR_LIST, SearchField.SEARCH_TYPE.FULL));
-        searchFields.add(new SearchField("agentHost", "agentHost",
-                SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.PARTIAL));
+        searchFields.add(new SearchField("cluster", "cluster", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("zoneName", "zoneName", SearchField.DATA_TYPE.STR_LIST, SearchField.SEARCH_TYPE.FULL));
+        searchFields.add(new SearchField("agentHost", "agentHost", SearchField.DATA_TYPE.STRING, SearchField.SEARCH_TYPE.PARTIAL));
 
-        sortFields.add(new SortField("eventTime", "evtTime", true,
-                SortField.SORT_ORDER.DESC));
+        sortFields.add(new SortField("eventTime", "evtTime", true, SortField.SORT_ORDER.DESC));
         sortFields.add(new SortField("policyId", "policy", false, SORT_ORDER.ASC));
         sortFields.add(new SortField("requestUser", "reqUser", false, SORT_ORDER.ASC));
         sortFields.add(new SortField("resourceType", "resType", false, SORT_ORDER.ASC));
@@ -125,8 +103,8 @@ public class AccessAuditsService {
             List<String> excludeUsersList = getExcludeUsersList();
             if (CollectionUtils.isNotEmpty(excludeUsersList)) {
                 Object oldUserExclusions = paramList.get("-requestUser");
-                if (oldUserExclusions instanceof Collection && (!((Collection<?>)oldUserExclusions).isEmpty())) {
-                    excludeUsersList.addAll((Collection<String>)oldUserExclusions);
+                if (oldUserExclusions instanceof Collection && (!((Collection<?>) oldUserExclusions).isEmpty())) {
+                    excludeUsersList.addAll((Collection<String>) oldUserExclusions);
                     paramList.put("-requestUser", excludeUsersList);
                 } else {
                     paramList.put("-requestUser", excludeUsersList);
@@ -140,7 +118,7 @@ public class AccessAuditsService {
         List<String> excludeUsersList = new ArrayList<String>(getServiceUserList());
 
         //for excluding additional users using new property in ranger-admin-site
-        String additionalExcludeUsers = PropertiesUtil.getProperty("ranger.accesslogs.exclude.users.list");
+        String       additionalExcludeUsers     = PropertiesUtil.getProperty("ranger.accesslogs.exclude.users.list");
         List<String> additionalExcludeUsersList = null;
         if (StringUtils.isNotBlank(additionalExcludeUsers)) {
             additionalExcludeUsersList = new ArrayList<>(Arrays.asList(StringUtils.split(additionalExcludeUsers, ",")));
@@ -154,12 +132,12 @@ public class AccessAuditsService {
     }
 
     private List<String> getServiceUserList() {
-        String components = EmbeddedServiceDefsUtil.DEFAULT_BOOTSTRAP_SERVICEDEF_LIST;
+        String       components       = EmbeddedServiceDefsUtil.DEFAULT_BOOTSTRAP_SERVICEDEF_LIST;
         List<String> serviceUsersList = new ArrayList<String>();
-        List<String> componentNames =  Arrays.asList(StringUtils.split(components,","));
-        for(String componentName : componentNames) {
-            String serviceUser = PropertiesUtil.getProperty("ranger.plugins."+componentName+".serviceuser");
-            if(StringUtils.isNotBlank(serviceUser)) {
+        List<String> componentNames   = Arrays.asList(StringUtils.split(components, ","));
+        for (String componentName : componentNames) {
+            String serviceUser = PropertiesUtil.getProperty("ranger.plugins." + componentName + ".serviceuser");
+            if (StringUtils.isNotBlank(serviceUser)) {
                 serviceUsersList.add(serviceUser);
             }
         }
