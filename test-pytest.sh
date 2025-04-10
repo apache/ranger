73c08ecf7b01c3dca95a6b7cd61bb487e6a15e6b
Chinmay Hegde <hegdechinmay24@gmail.com>
2025-04-10 22:31:12 +0530

    RANGER-5175: Made test-pytest.sh script file to be dynamic

--- test-pytest.sh
+++ test-pytest.sh
@@ -1,46 +1,72 @@
 #!/bin/bash
 
-# Remove all containers
-docker rm -f $(docker ps -aq)
+#handle input
+if [ $# -lt 1 ]; then
+  echo "no arguments passed , using default DB: postgres"
+  DB_TYPE=postgres
+  EXTRA_SERVICES=()
 
+else
+  DB_TYPE=$1
+  shift
+  EXTRA_SERVICES=("$@")
+fi
+
+# Remove all containers and clean up docker space
+docker rm -f $(docker ps -aq --filter "name=ranger")
+docker system prune --all --force --volumes
+
+#path setup
 RANGER_DOCKER_PATH="dev-support/ranger-docker"
-TESTS_PATH="PyTest-KMS-HDFS"
-cd "$RANGER_DOCKER_PATH"
+TESTS_PATH="$HOME/Desktop/PyTest-KMS-EP"
+cd "$RANGER_DOCKER_PATH"|| exit 1
 
 # Download archives
-./download-archives.sh hadoop hbase 
-
-docker system prune --all --force --volumes
+if [${#EXTRA_SERVICES[@]} -gt 0]; then
+  ./download-archives.sh "${EXTRA_SERVICES[@]}"
+fi
 
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
-export RANGER_DB_TYPE=postgres
+export RANGER_DB_TYPE="$DB_TYPE"
 
 # Build base image
-docker-compose -f docker-compose.ranger-base.yml build --no-cache
+docker-compose -f docker-compose.ranger-base.yml build
 
 # Build Apache Ranger
 docker-compose -f docker-compose.ranger-base.yml -f docker-compose.ranger-build.yml up --build
 
-# Bring up all services
-docker compose -f docker-compose.ranger-${RANGER_DB_TYPE}.yml \
-  -f docker-compose.ranger.yml \
-  -f docker-compose.ranger-usersync.yml \
-  -f docker-compose.ranger-tagsync.yml \
-  -f docker-compose.ranger-kms.yml \
-  -f docker-compose.ranger-hadoop.yml \
-  -f docker-compose.ranger-hbase.yml up -d --build
+# Bring up basic services
+DOCKER_FILES=(
+  "-f" "docker-compose.ranger-${RANGER_DB_TYPE}.yml"
+  "-f" "docker-compose.ranger.yml"
+  "-f" "docker-compose.ranger-usersync.yml"
+  "-f" "docker-compose.ranger-tagsync.yml"
+  "-f" "docker-compose.ranger-kms.yml"
+  )
+
+# add extra service from input
+for service in "${EXTRA_SERVICES[@]}" ; do
+  DOCKER_FILES+=("-f" "docker-compose.ranger-${service}.yml")
+done
+
+#start containers
+docker compose "${DOCKER_FILES[@]}" up -d --build
 
 echo "🕐 Waiting for containers to start..."
 sleep 60
 
 echo "✅ Checking container status..."
-containers=(
-  ranger ranger-zk ranger-solr ranger-postgres ranger-usersync
-  ranger-tagsync ranger-kms ranger-hadoop ranger-hbase
-)
+BASE_SERVICES=(ranger ranger-${RANGER_DB_TYPE} ranger-zk ranger-solr ranger-usersync ranger-tagsync ranger-kms)
+ALL_SERVICES=("${BASE_SERVICES[@]}")
+
+for service in "${EXTRA_SERVICES[@]}"; do
+  ALL_SERVICES+=("ranger-${service}")
+done
+
+
 flag=true
-for container in "${containers[@]}"; do
+for container in "${ALL_SERVICES[@]}"; do
   if [[ $(docker inspect -f '{{.State.Running}}' "$container" 2>/dev/null) == "true" ]]; then
     echo "✔️ Container $container is running!"
   else
@@ -49,24 +75,25 @@ for container in "${containers[@]}"; do
   fi
 done
 
+#RUN TESTS--------
 if [[ $flag == true ]]; then
   echo "🚀 All required containers are up. Running test cases..."
-  cd "$TESTS_PATH"  # Switch to the tests directory
+  cd "$TESTS_PATH" || exit 1         # Switch to the tests directory
 
   python -m venv myenv  # Create a new environment
   source myenv/bin/activate  # Activate it
   pip install -r requirements.txt  # Install dependencies
 
-  pytest -vs test_kms/ --html=report_kms.html   # Runs all tests in the tests directory with report generation included
+  pytest -vs test_kms/ --html=report_kms.html  # Runs all tests in the tests directory
   pytest -vs test_hdfs/ --html=report_hdfs.html
 else
   echo "⚠️ Some containers failed to start. Exiting..."
-  docker stop $(docker ps -q) && docker rm $(docker ps -aq)
+  docker stop $(docker ps -q --filter "name=ranger") && docker rm $(docker ps -aq --filter "name=ranger")
   exit 1
 fi
 
 echo "🧹 Cleaning up containers..."
-docker stop $(docker ps -q) && docker rm $(docker ps -aq)
+docker stop $(docker ps -q --filter "name=ranger") && docker rm $(docker ps -aq --filter "name=ranger")
 
 # Open the generated reports
 echo " Opening test reports..."
@@ -80,3 +107,4 @@ fi
 
 echo "✅ Test execution complete and environment cleaned up!"
 exit  0
+
