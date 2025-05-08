92b7bec16689e1d4593fec3f094563c920c84aea
Vyom Mani Tiwari <vtiwari@cloudera.com>
2025-05-08 09:02:40 +0530

    fixed the copywrite and some small modification to test

--- agents-common/src/test/java/org/apache/ranger/plugin/policyengine/RangerBasePluginTest.java
+++ agents-common/src/test/java/org/apache/ranger/plugin/policyengine/RangerBasePluginTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.ranger.plugin.policyengine;
 
 import com.google.gson.Gson;
@@ -68,6 +87,8 @@ public class RangerBasePluginTest {
         pluginConfig.addResourceIfReadable(FileSystems.getDefault().getPath(basedir, RANGER_DEFAULT_AUDIT_CONF).toString());
         pluginConfig.addResourceIfReadable(FileSystems.getDefault().getPath(basedir, RANGER_DEFAULT_SECURITY_CONF).toString());
         pluginConfig.addResourceIfReadable(FileSystems.getDefault().getPath(basedir, RANGER_DEFAULT_POLICY_MGR_SSL_CONF).toString());
+        pluginConfig.getProperties().put("ranger.plugin.hbase.supports.in.place.policy.updates", "true");
+        pluginConfig.getProperties().put("ranger.plugin.hbase.supports.policy.deltas", "true");
 
         rangerBasePlugin = new RangerBasePlugin(pluginConfig);
         rangerBasePlugin.init();
@@ -112,7 +133,7 @@ public class RangerBasePluginTest {
                     }
                 } catch (Error e) {
                     // The PolicyRefresher modifies the policy, so it's expected that tests for the modified policy would fail.
-                    if (test.result.getPolicyId() == 821 || test.result.getPolicyId() == 822) {
+                    if (test.result.getPolicyId() == 821) {
                         failedCount++;
                     } else {
                         throw e;
