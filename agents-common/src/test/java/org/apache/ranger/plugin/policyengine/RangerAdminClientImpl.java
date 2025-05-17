982c133fbb44536b79f18dc0ffff7187807a66ec
Madhan Neethiraj <madhan@apache.org>
2025-05-17 11:39:19 -0700

    Update RangerAdminClientImpl.java

--- agents-common/src/test/java/org/apache/ranger/plugin/policyengine/RangerAdminClientImpl.java
+++ agents-common/src/test/java/org/apache/ranger/plugin/policyengine/RangerAdminClientImpl.java
@@ -25,56 +25,57 @@ import java.nio.file.Files;
 
 public class RangerAdminClientImpl
         extends AbstractRangerAdminClient {
-    private static final String RANGER_TEST_BASEDIR = "RANGER_TEST_BASEDIR";
-    private static final String RANGER_RESOURCE_BASE = "RANGER_RESOURCE_BASE";
-    private static final String policiesFilepath = "/src/test/resources/policyengine/hbase-test-policies.json";
+    private static final String RANGER_TEST_BASEDIR     = "RANGER_TEST_BASEDIR";
+    private static final String RANGER_RESOURCE_BASE    = "RANGER_RESOURCE_BASE";
+    private static final String policiesFilepath        = "/src/test/resources/policyengine/hbase-test-policies.json";
     private static final String updatedPoliciesFilepath = "/src/test/resources/policyengine/updated-hbase-test-policies.json";
-    private static final String serviceDefFilename = "/src/test/resources/policyengine/ranger-servicedef-hbase.json";
-    private static final String tagServiceDefFilename = "/src/test/resources/policyengine/ranger-servicedef-tag.json";
+    private static final String serviceDefFilename      = "/src/test/resources/policyengine/ranger-servicedef-hbase.json";
+    private static final String tagServiceDefFilename   = "/src/test/resources/policyengine/ranger-servicedef-tag.json";
 
     @SuppressWarnings("unused")
     private String serviceName;
+
     @SuppressWarnings("unused")
     private String appId;
+
     private volatile boolean isLoadPolicyDelta;
 
     @Override
     public void init(String serviceName, String appId, String configPropertyPrefix, Configuration config) {
         super.init(serviceName, appId, configPropertyPrefix, config);
+
         this.serviceName = serviceName;
-        this.appId = appId;
+        this.appId       = appId;
     }
 
     @Override
     public ServicePolicies getServicePoliciesIfUpdated(long lastKnownVersion, long lastActivationTimeInMillis)
             throws Exception {
         String basedir = System.getProperty(RANGER_TEST_BASEDIR);
+
         if (basedir == null) {
             basedir = new File(".").getCanonicalPath();
         }
-        String resourceBasePath = System.getProperty(RANGER_RESOURCE_BASE);
-        if (resourceBasePath == null) {
-            resourceBasePath = ".";
-        }
-        byte[] policiesBytes;
-        if (!isLoadPolicyDelta) {
-            policiesBytes = Files.readAllBytes(FileSystems.getDefault().getPath(basedir, policiesFilepath));
-        } else {
-            policiesBytes = Files.readAllBytes(FileSystems.getDefault().getPath(basedir, updatedPoliciesFilepath));
-        }
-        ServicePolicies ret = gson.fromJson(new String(policiesBytes, Charset.defaultCharset()), ServicePolicies.class);
-        byte[] serviceDefBytes = Files.readAllBytes(FileSystems.getDefault().getPath(basedir, serviceDefFilename));
-        byte[] tagServiceDefBytes = Files.readAllBytes(FileSystems.getDefault().getPath(basedir, tagServiceDefFilename));
-        RangerServiceDef serviceDef = gson.fromJson(new String(serviceDefBytes, Charset.defaultCharset()), RangerServiceDef.class);
-        RangerServiceDef tagServiceDef = gson.fromJson(new String(tagServiceDefBytes, Charset.defaultCharset()), RangerServiceDef.class);
+
+        byte[]           policiesBytes      = Files.readAllBytes(FileSystems.getDefault().getPath(basedir, isLoadPolicyDelta ? updatedPoliciesFilepath : policiesFilepath));
+        byte[]           serviceDefBytes    = Files.readAllBytes(FileSystems.getDefault().getPath(basedir, serviceDefFilename));
+        byte[]           tagServiceDefBytes = Files.readAllBytes(FileSystems.getDefault().getPath(basedir, tagServiceDefFilename));
+        ServicePolicies  ret                = gson.fromJson(new String(policiesBytes, Charset.defaultCharset()), ServicePolicies.class);
+        RangerServiceDef serviceDef         = gson.fromJson(new String(serviceDefBytes, Charset.defaultCharset()), RangerServiceDef.class);
+        RangerServiceDef tagServiceDef      = gson.fromJson(new String(tagServiceDefBytes, Charset.defaultCharset()), RangerServiceDef.class);
+
         ret.setServiceDef(serviceDef);
+
         if (ret.getTagPolicies() == null) {
             ret.setTagPolicies(new ServicePolicies.TagPolicies());
         }
+
         ret.getTagPolicies().setServiceDef(tagServiceDef);
+
         if (!isLoadPolicyDelta) {
             isLoadPolicyDelta = true;
         }
+
         return ret;
     }
 }
