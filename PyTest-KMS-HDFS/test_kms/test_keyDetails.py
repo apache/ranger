dd5de83986c90a59966421815835e9264bf337da
Chinmay Hegde <hegdechinmay24@gmail.com>
2025-05-19 18:47:03 +0530

    RANGER-5175:Added some missing test cases to test_keyDetails.py

--- PyTest-KMS-HDFS/test_kms/test_keyDetails.py
+++ PyTest-KMS-HDFS/test_kms/test_keyDetails.py
@@ -10,7 +10,7 @@ class TestKeyDetails:
     @pytest.fixture(autouse=True)
     def setup_class(self, create_test_key):
         self.test_key = create_test_key
-
+    
     # ***********************************************************************************
     #  Get key names 
     # ***********************************************************************************
@@ -27,73 +27,117 @@ class TestKeyDetails:
 
     # ***********************************************************************************
     #  Parametrized Get key metadata check for existent and non existent key
+    #  Note: key1 is coming from create_test_key fixture in conftest.py
     # ***********************************************************************************
+
     @pytest.mark.parametrize("key_name, expected_status, expected_response", [
-    ("my_key", 200, "valid"),                   # Key exists, should return valid metadata
-    ("non-existent-key", 200, "invalid"),       # Key does not exist but returns 200 with [] should give 404
+    ("key1", 200, "valid"),                   # Key exists, should return valid metadata
+    ("non-existent-key", 200, "invalid"),     # Key does not exist but returns 200 with [] should give 404
     ])
     def test_get_key_metadata(self, headers, key_name, expected_status, expected_response):
     
         response = requests.get(f"{BASE_URL}/key/{key_name}/_metadata", headers=headers, params=PARAMS)
- 
-        if response.status_code!=expected_status:                  #log check
-            logs=fetch_logs()           
-            pytest.fail(f"Get key metadata operation failed. API Response: {response.text}\nLogs:\n{logs}")
+        
+        logs=fetch_logs()                #log check
+        assert response.status_code==expected_status,f"Get key metadata operation failed. API Response: {response.text}\nLogs:\n{logs}"           
 
         if expected_response == "invalid":
          assert response.text.strip() in ["", "[ ]", "{ }"], f"Expected blank response for non-existent key, got: {response.text}"
+        
 
-        print(response.json())
+
 
     # ***********************************************************************************
     #  Parametrized Get Key version for existent and non existent key
+    #  Note: key1 is coming from create_test_key fixture in conftest.py
     # ***********************************************************************************
+
     @pytest.mark.parametrize("key_name, expected_status, expected_response", [
-        ("my_key", 200, "valid"),             # Key exists
-        ("non-existent-key", 200,"invalid"),  # Misleading response for non-existent key gives 200 should've given 404
+        ("key1", 200, "valid"),               # Key exists
+        ("non-existent-key", 200,"invalid"),  # Key does not exist but returns 200 with [] should give 404
     ])
     def test_get_key_versions(self, headers, key_name, expected_status,expected_response):
 
         response = requests.get(f"{BASE_URL}/key/{key_name}/_versions", headers=headers, params=PARAMS)
-         
-        if response.status_code != expected_status:          #log check
-            logs=fetch_logs()           
-            pytest.fail(f"Get key version operation failed. API Response: {response.text}\nLogs:\n{logs}")
+        
+        logs=fetch_logs()    #log check
+        assert response.status_code == expected_status,f"Get key version operation failed. API Response: {response.text}\nLogs:\n{logs}"        
           
         if expected_response == "invalid":
          assert response.text.strip() in ["", "[ ]", "{ }"], f"Expected blank response for non-existent key, got: {response.text}"
 
 
+    # ***********************************************************************************
+    #  Get Key metadata for multiple keys at once
+    #  Note: key1 is coming from create_test_key fixture in conftest.py
+    # ***********************************************************************************
+
+    def test_get_keys_metadata(self, headers):
+       
+       #Create second key (key2)
+        key_name="key2"
+        data = {
+            "name":key_name
+        }
+        create_response = requests.post(f"{BASE_URL}/keys", headers=headers, json=data, params=PARAMS)
+        assert create_response.status_code == 201, f"Key2 creation failed: {create_response.text}"
+
+        try:
+           # Check metadata for existing keys (key1 and key2)
+            existing_keys = ["key1", "key2"]
+            params = [("key", k) for k in existing_keys]
+            params.append(("user.name", "keyadmin"))
+
+            response = requests.get(f"{BASE_URL}/keys/metadata", headers=headers, params=params)
+            assert response.status_code == 200, f"Metadata fetch failed: {response.status_code}"
+
+            metadata = response.json()
+            returned_keys = [entry["name"] for entry in metadata]
+            for key in existing_keys:
+                assert key in returned_keys, f"Expected key '{key}' not found in metadata response"
+
+            # Check metadata for non-existent keys
+            fake_keys = ["nonExistent_key_1", "nonExistent_key_2"]
+            params = [("key", k) for k in fake_keys]
+            params.append(("user.name", "keyadmin"))
+
+            response = requests.get(f"{BASE_URL}/keys/metadata", headers=headers, params=params)
+            assert response.status_code == 200, f"Metadata fetch failed for non-existent keys: {response.status_code}"
+
+            assert response.json() == [{}, {}], f"Expected blank response for non-existent key, got: {response.text}"
+
+        finally:
+            # Cleanup key2
+            requests.delete(f"{BASE_URL}/key/{key_name}", params=PARAMS)
 
 
 
 
+    # ***********************************************************************************
+    #  Test for endpoint 'get key version' 
+    #  Note: key1 is coming from create_test_key fixture in conftest.py
+    # ***********************************************************************************
+    
+    @pytest.mark.parametrize("version_name, expected_status, expected_valid", [
+    ("key1@0", 200, True),                      # Valid version
+    ("non-existent-key@0", 200, False),         # Key does not exist but returns 200 with [] should give 404
+    ])
+
+    def test_get_key_version(self, headers, version_name, expected_status, expected_valid):
+        response = requests.get(f"{BASE_URL}/keyversion/{version_name}", headers=headers, params=PARAMS)
+        
+        logs = fetch_logs()
+        assert response.status_code == expected_status,f"Get key version failed. Response: {response.text}\nLogs:\n{logs}"
+        
+        if expected_valid:
+            try:
+                version_data = response.json()
+                assert "versionName" in version_data, "versionName missing in response"
+                assert version_data["versionName"] == version_name, f"Mismatch in version name: expected {version_name}"
+            except ValueError:
+                pytest.fail(f"Expected valid JSON response, got: {response.text}")
+        else:
+            assert response.text.strip() in [" ", "{ }", "[ ]"], f"Expected empty for invalid version, got: {response.text}"
+               
 
 
-
-
-
-
-
-
-
-
-
-        # try:
-        #  json_response = response.json()
-        # except requests.exceptions.JSONDecodeError:
-         
-        #  json_response = response.text  # Store raw text response instead
-
-        # if expected_response == "invalid":
-        #     assert json_response =="key not found", f"Unexpected response: {json_response}"
-
-
-   
-        #this below was in testKeyMetadata-------------
-         # response_data = response.text                      #The error occurs because the API returns plain text ("key not found") instead of a JSON response when querying a non-existent key.
-                                                           #works fine if text is used instead json bcoz of my changes
-        # if expected_response == "valid":
-        #     assert response_data != "key not found"
-        # elif expected_response == "invalid":
-        #     assert response_data == "key not found"
