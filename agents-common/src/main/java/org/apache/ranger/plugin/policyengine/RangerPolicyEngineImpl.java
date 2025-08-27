b15046ed574004506f9ae8bb852ccc5cadf0ee57
Madhan Neethiraj <madhan@apache.org>
2025-08-27 16:07:32 -0700

    RANGER-5295: fix GDS policy evaluation handling of row-filters

--- agents-common/src/main/java/org/apache/ranger/plugin/policyengine/RangerPolicyEngineImpl.java
+++ agents-common/src/main/java/org/apache/ranger/plugin/policyengine/RangerPolicyEngineImpl.java
@@ -1072,12 +1072,35 @@ public class RangerPolicyEngineImpl implements RangerPolicyEngine {
 
         if (gdsResult != null) {
             if (result.getPolicyType() == RangerPolicy.POLICY_TYPE_ACCESS) {
+                // pick access result from GDS policies only if there is no decision yet
                 if (!result.getIsAccessDetermined() && gdsResult.getIsAllowed()) {
                     result.setIsAllowed(true);
                     result.setIsAccessDetermined(true);
                     result.setPolicyId(gdsResult.getPolicyId());
                     result.setPolicyVersion(gdsResult.getPolicyVersion());
                     result.setPolicyPriority(RangerPolicy.POLICY_PRIORITY_NORMAL);
+                }
+            } else if (result.getPolicyType() == RangerPolicy.POLICY_TYPE_ROWFILTER) {
+                // pick row-filter from GDS policies only if there is no decision yet
+                if (result.getPolicyId() != -1 && CollectionUtils.isNotEmpty(gdsResult.getRowFilters())) {
+                    result.setIsAllowed(true);
+                    result.setIsAccessDetermined(true);
+                    result.setPolicyId(gdsResult.getPolicyId());
+                    result.setPolicyVersion(gdsResult.getPolicyVersion());
+                    result.setPolicyPriority(RangerPolicy.POLICY_PRIORITY_NORMAL);
+                    result.setFilterExpr(gdsResult.getRowFilters().get(0));
+                }
+            } else if (result.getPolicyType() == RangerPolicy.POLICY_TYPE_DATAMASK) {
+                // pick data-mask from GDS policies only if there is no decision yet
+                if (result.getPolicyId() != -1 && StringUtils.isNotEmpty(gdsResult.getMaskType())) {
+                    result.setIsAllowed(true);
+                    result.setIsAccessDetermined(true);
+                    result.setPolicyId(gdsResult.getPolicyId());
+                    result.setPolicyVersion(gdsResult.getPolicyVersion());
+                    result.setPolicyPriority(RangerPolicy.POLICY_PRIORITY_NORMAL);
+                    result.setMaskType(gdsResult.getMaskType());
+                    result.setMaskedValue(gdsResult.getMaskedValue());
+                    result.setMaskCondition(gdsResult.getMaskCondition());
                 }
             }
 
