f7929ef82b647cff38c12c65ef21db4582fe13c8
Madhan Neethiraj <madhan@apache.org>
2025-09-03 15:04:13 -0700

    RANGER-5309: add authz-api module

---
+++ authz-api/src/main/java/org/apache/ranger/authz/api/RangerAuthzApiErrorCode.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.ranger.authz.api;
+
+public enum RangerAuthzApiErrorCode implements RangerAuthzErrorCode {
+    AUTHZ_FACTORY_NOT_INITIALIZED(500, "00-001", "RangerAuthorizerFactory not initialized"),
+    AUTHZ_FACTORY_INITIALIZATION_FAILED(500, "00-002", "Initialization of authorizer factory failed"),
+
+    INVALID_REQUEST_SERVICE_NOT_FOUND(400, "00-001", "{0}: service not found"),
+    INVALID_REQUEST_SERVICE_TYPE_NOT_FOUND(400, "00-002", "{0}: service type not found"),
+    INVALID_REQUEST_RESOURCE_TYPE_NOT_FOUND(400, "00-003", "{0}: resource type not found"),
+    INVALID_REQUEST_PERMISSION_NOT_FOUND(400, "00-004", "{0}: permission not found"),
+    INVALID_REQUEST_SERVICE_NAME_OR_TYPE_MANDATORY(400, "00-005", "Service name or service type is mandatory"),
+    INVALID_RESOURCE_TEMPLATE_UNEXPECTED_MARKER_AT(400, "00-006", "Invalid resource template: {0}. Unexpected marker '{1}' at position {2}");
+
+    private static final String ERROR_CODE_MODULE_PREFIX = "AUTHZ";
+
+    private final int    httpStatusCode;
+    private final String code;
+    private final String message;
+
+    RangerAuthzApiErrorCode(int httpStatusCode, String code, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.code           = String.format("%s-%3d-%s", ERROR_CODE_MODULE_PREFIX, String.valueOf(httpStatusCode), code);
+        this.message        = message;
+    }
+
+    public int getHttpStatusCode() {
+        return httpStatusCode;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "RangerAuthzBaseErrorCode{" +
+                "httpStatusCode=" + httpStatusCode +
+                ", code='" + code + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ authz-api/src/main/java/org/apache/ranger/authz/model/RangerUserInfo.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.ranger.authz.model;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Map;
+import java.util.Set;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RangerUserInfo {
+    private String              name;
+    private Map<String, Object> attributes;
+    private Set<String>         groups;
+    private Set<String>         roles;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public Set<String> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(Set<String> groups) {
+        this.groups = groups;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "RangerUserInfo{" +
+                "name='" + name + '\'' +
+                ", attributes=" + attributes +
+                ", groups=" + groups +
+                ", roles=" + roles +
+                '}';
+    }
+}
