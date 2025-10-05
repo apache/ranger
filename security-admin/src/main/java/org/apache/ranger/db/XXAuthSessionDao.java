d6ce6687e7fa6c014d1933e19fb10f51ca9640e6
Dhaval Rajpara <dhavalrajpara1304@gmail.com>
2025-10-05 06:55:36 +0530

    RANGER-5271 : Last login date and time on Ranger Home page (#655)

--- security-admin/src/main/java/org/apache/ranger/db/XXAuthSessionDao.java
+++ security-admin/src/main/java/org/apache/ranger/db/XXAuthSessionDao.java
@@ -71,6 +71,23 @@ public class XXAuthSessionDao extends BaseDao<XXAuthSession> {
 		}
 	}
 
+    public XXAuthSession getLastSuccessLoginAuthSessionByUserId(String loginId) {
+        if (loginId == null) {
+            return null;
+        }
+        try {
+            List<XXAuthSession> sessions = getEntityManager()
+                    .createNamedQuery("XXAuthSession.getSuccessAuthSessionsByUserId", tClass)
+                    .setParameter("loginId", loginId)
+                    .setParameter("authStatus", XXAuthSession.AUTH_STATUS_SUCCESS)
+                    .setMaxResults(2)
+                    .getResultList();
+            return sessions.size() >= 2 ? sessions.get(1) : null;
+        } catch (NoResultException ignoreNoResultFound) {
+            return null;
+        }
+    }
+
 	public long getRecentAuthFailureCountByLoginId(String loginId, int timeRangezSecond){
 		Date authWindowStartTime = new Date(DateUtil.getUTCDate().getTime() - timeRangezSecond * 1000);
 
