971a896fe46a1a22c2602fa5992f3adde67f994a
Rakesh Gupta <rakesh.gupta.dev264@gmail.com>
2025-12-30 16:06:49 +0530

    RANGER-5212: Search filter in Roles tab does not consider all relevant role columns

--- security-admin/src/main/java/org/apache/ranger/biz/RoleDBStore.java
+++ security-admin/src/main/java/org/apache/ranger/biz/RoleDBStore.java
@@ -299,68 +299,45 @@ public class RoleDBStore implements RoleStore {
     }
 
     public RangerRoleList getRoles(SearchFilter filter, RangerRoleList rangerRoleList) throws Exception {
-        List<RangerRole> roles   = new ArrayList<>();
-        List<XXRole>     xxRoles = roleService.searchResources(filter, roleService.searchFields, roleService.sortFields, rangerRoleList);
+        List<RangerRole> roles = getRoles(filter);
 
-        if (CollectionUtils.isNotEmpty(xxRoles)) {
-            for (XXRole xxRole : xxRoles) {
-                roles.add(roleService.read(xxRole.getId()));
-            }
-        }
-
-        rangerRoleList.setRoleList(roles);
+        setPaginatedResult(roles, filter, rangerRoleList);
 
         return rangerRoleList;
     }
 
-    public RangerRoleList getRolesForUser(SearchFilter filter, RangerRoleList rangerRoleList) {
-        List<RangerRole> roles       = new ArrayList<>();
+    public RangerRoleList getRolesForUser(SearchFilter filter, RangerRoleList rangerRoleList) throws Exception {
+        if (filter == null) {
+            filter = new SearchFilter();
+        }
+
+        List<RangerRole> roles;
         UserSessionBase  userSession = ContextUtil.getCurrentUserSession();
 
         if (userSession != null && userSession.getUserRoleList().size() == 1 && userSession.getUserRoleList().contains(RangerConstants.ROLE_USER) && userSession.getLoginId() != null) {
             VXUser       loggedInVXUser = xUserService.getXUserByUserName(userSession.getLoginId());
             List<XXRole> xxRoles        = daoMgr.getXXRole().findByUserId(loggedInVXUser.getId());
 
+            roles = new ArrayList<>();
+
             if (CollectionUtils.isNotEmpty(xxRoles)) {
                 for (XXRole xxRole : xxRoles) {
                     roles.add(roleService.read(xxRole.getId()));
                 }
             }
 
-            if (predicateUtil != null && filter != null && !filter.isEmpty()) {
+            if (predicateUtil != null && !filter.isEmpty()) {
                 List<RangerRole> copy = new ArrayList<>(roles);
 
                 predicateUtil.applyFilter(copy, filter);
 
                 roles = copy;
             }
-
-            int totalCount = roles.size();
-            int startIndex = filter.getStartIndex();
-            int pageSize   = filter.getMaxRows();
-            int toIndex    = Math.min(startIndex + pageSize, totalCount);
-
-            if (CollectionUtils.isNotEmpty(roles)) {
-                roles = roles.subList(startIndex, toIndex);
-
-                rangerRoleList.setResultSize(roles.size());
-                rangerRoleList.setPageSize(filter.getMaxRows());
-                rangerRoleList.setSortBy(filter.getSortBy());
-                rangerRoleList.setSortType(filter.getSortType());
-                rangerRoleList.setStartIndex(filter.getStartIndex());
-                rangerRoleList.setTotalCount(totalCount);
-            }
         } else {
-            List<XXRole> xxRoles = roleService.searchResources(filter, roleService.searchFields, roleService.sortFields, rangerRoleList);
-
-            if (CollectionUtils.isNotEmpty(xxRoles)) {
-                for (XXRole xxRole : xxRoles) {
-                    roles.add(roleService.read(xxRole.getId()));
-                }
-            }
+            roles = getRoles(filter);
         }
 
-        rangerRoleList.setRoleList(roles);
+        setPaginatedResult(roles, filter, rangerRoleList);
 
         return rangerRoleList;
     }
@@ -498,6 +475,26 @@ public class RoleDBStore implements RoleStore {
         Long roleRefZoneCount = daoMgr.getXXSecurityZoneRefRole().findRoleRefZoneCount(roleName);
 
         return roleRefZoneCount < 1;
+    }
+
+    private void setPaginatedResult(List<RangerRole> roles, SearchFilter filter, RangerRoleList result) {
+        int totalCount = roles.size();
+        int startIndex = filter.getStartIndex();
+        int pageSize   = filter.getMaxRows();
+        int toIndex    = Math.min(startIndex + pageSize, totalCount);
+
+        if (CollectionUtils.isNotEmpty(roles)) {
+            roles = roles.subList(startIndex, toIndex);
+
+            result.setResultSize(roles.size());
+            result.setPageSize(filter.getMaxRows());
+            result.setSortBy(filter.getSortBy());
+            result.setSortType(filter.getSortType());
+            result.setStartIndex(filter.getStartIndex());
+            result.setTotalCount(totalCount);
+        }
+
+        result.setRoleList(roles);
     }
 
     public static class RoleVersionUpdater implements Runnable {
