3bb20cc5d2b60429c21c9dd687cbc459fa0efa64
PradeeP AgrawaL <pradeep@apache.org>
2025-12-31 13:37:18 +0530

    Revert "RANGER-5397: IllegalArgumentException in RangerRESTClient when no ran…"
    This reverts commit 464bfd95efa37984a0274bcdf7c0332330f4cc57.

--- agents-common/src/main/java/org/apache/ranger/plugin/util/RangerRESTClient.java
+++ agents-common/src/main/java/org/apache/ranger/plugin/util/RangerRESTClient.java
@@ -110,11 +110,9 @@ public class RangerRESTClient {
         mUrl               = url;
         mSslConfigFileName = sslConfigFileName;
         configuredURLs     = StringUtil.getURLs(mUrl);
-        if (StringUtil.isEmpty(url)) {
-            throw new IllegalArgumentException("Ranger URL is null or empty. Likely caused by incorrect configuration");
-        } else {
-            setLastKnownActiveUrlIndex((new Random()).nextInt(getConfiguredURLs().size()));
-        }
+
+        setLastKnownActiveUrlIndex((new Random()).nextInt(getConfiguredURLs().size()));
+
         init(config);
     }
 
