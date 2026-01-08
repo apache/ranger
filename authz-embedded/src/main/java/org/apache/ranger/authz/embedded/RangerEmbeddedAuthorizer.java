4374e59cb689b6ea972822d10e3c3f7492824af4
Madhan Neethiraj <madhan@apache.org>
2026-01-07 20:03:52 -0800

    Update RangerEmbeddedAuthorizer.java

--- authz-embedded/src/main/java/org/apache/ranger/authz/embedded/RangerEmbeddedAuthorizer.java
+++ authz-embedded/src/main/java/org/apache/ranger/authz/embedded/RangerEmbeddedAuthorizer.java
@@ -56,6 +56,7 @@ public class RangerEmbeddedAuthorizer extends RangerAuthorizer {
         this.config = new RangerAuthzConfig(properties);
     }
 
+    @Override
     public void init() throws RangerAuthzException {
         AuditProviderFactory.getInstance().init(config.getAuditProperties(), "ranger-authz");
 
