c7c35b25e02a8c203ea60994489b7793a4a82638
kimsehwan96 <sktpghks138@gmail.com>
2026-01-28 22:11:59 +0900

    RANGER-5463: Return boolean from createOrDeleteXGroupUsers() to track actual updates

--- security-admin/src/main/java/org/apache/ranger/biz/XUserMgr.java
+++ security-admin/src/main/java/org/apache/ranger/biz/XUserMgr.java
@@ -2959,10 +2959,14 @@ public class XUserMgr extends XUserMgrBase {
                 }
 
                 for (GroupUserInfo groupUserInfo : groupUserInfoList) {
-                    xGroupUserService.createOrDeleteXGroupUsers(groupUserInfo, usersFromDB);
+                    if (xGroupUserService.createOrDeleteXGroupUsers(groupUserInfo, usersFromDB)) {
+                        updatedGroups++;
+                    }
+                    
                 }
-
-                updatedGroups = groupUserInfoList.size();
+                if (logger.isDebugEnabled()) {
+                    logger.debug("No. of groups actually updated = {}", updatedGroups);
+                }
             }
 			if (updatedGroups > 0) {
 				updateUserStoreVersion("createOrDeleteXGroupUserList(updatedGroups=" + updatedGroups + ")");
