0f73972aa59b259045c8ef5ba4f8c15ea6ffddad
Vyom Mani Tiwari <vyommani@gmail.com>
2026-03-23 12:41:59 +0530

    initial SAML implementation

---
+++ security-admin/src/main/java/org/apache/ranger/security/web/saml/RangerSamlRegistrationFactory.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.ranger.security.web.saml;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.security.converter.RsaKeyConverters;
+import org.springframework.security.saml2.core.Saml2X509Credential;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
+
+import java.io.FileInputStream;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+
+public class RangerSamlRegistrationFactory {
+    private RangerSamlRegistrationFactory(){}
+
+    public static RelyingPartyRegistration buildWithSigningCredential(RelyingPartyRegistration.Builder builder, String privateKeyPath, String certPath) throws Exception {
+        RSAPrivateKey privateKey = RsaKeyConverters.pkcs8().convert(new FileSystemResource(privateKeyPath).getInputStream());
+        X509Certificate certificate;
+        try (FileInputStream fis = new FileInputStream(certPath)) {
+            certificate = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(fis);
+        }
+        Saml2X509Credential signingCredential = Saml2X509Credential.signing(privateKey, certificate);
+        return builder.signingX509Credentials(c -> c.add(signingCredential)).build();
+    }
+}
