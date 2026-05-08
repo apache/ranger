9009703be065d0de7a9bd79672154cca17a67d5d
Ramesh Mani <rmani@apache.org>
2026-05-08 13:36:39 -0700

    RANGER-5577:API to support bulk delete of resources in DataShare - Fixed review comments

--- security-admin/src/main/java/org/apache/ranger/rest/GdsREST.java
+++ security-admin/src/main/java/org/apache/ranger/rest/GdsREST.java
@@ -1292,36 +1292,36 @@ public class GdsREST {
     @DELETE
     @Path("/resources")
     @PreAuthorize("@rangerPreAuthSecurityHandler.isAPIAccessible(\"" + RangerAPIList.REMOVE_SHARED_RESOURCES + "\")")
-    public void removeSharedResources(@QueryParam("resourceIds") List<Long> resourceIds) {
-        LOG.debug("==> GdsREST.removeSharedResources(resourceIds={})", resourceIds);
+    public void removeSharedResources(@QueryParam("id") List<Long> id) {
+        LOG.debug("==> GdsREST.removeSharedResources(resourceIds={})", id);
 
         RangerPerfTracer perf = null;
 
         try {
-            if (resourceIds == null) {
+            if (id == null) {
                 throw new Exception("resourceIds must not be null");
             }
 
-            if (resourceIds.size() > SHARED_RESOURCES_MAX_BATCH_SIZE) {
+            if (id.size() > SHARED_RESOURCES_MAX_BATCH_SIZE) {
                 throw new Exception("removeSharedResources batch size exceeded the configured limit: Maximum allowed is " + SHARED_RESOURCES_MAX_BATCH_SIZE);
             }
 
             if (RangerPerfTracer.isPerfTraceEnabled(PERF_LOG)) {
-                perf = RangerPerfTracer.getPerfTracer(PERF_LOG, "GdsREST.removeSharedResources(" + resourceIds + ")");
+                perf = RangerPerfTracer.getPerfTracer(PERF_LOG, "GdsREST.removeSharedResources(" + id + ")");
             }
 
-            gdsStore.removeSharedResources(resourceIds);
+            gdsStore.removeSharedResources(id);
         } catch (WebApplicationException excp) {
             throw excp;
         } catch (Throwable excp) {
-            LOG.error("removeSharedResources({}) failed", resourceIds, excp);
+            LOG.error("removeSharedResources({}) failed", id, excp);
 
             throw restErrorUtil.createRESTException(excp.getMessage());
         } finally {
             RangerPerfTracer.log(perf);
         }
 
-        LOG.debug("<== GdsREST.removeSharedResources(resourceIds={})", resourceIds);
+        LOG.debug("<== GdsREST.removeSharedResources(resourceIds={})", id);
     }
 
     @GET
