55d8587a7706dc4236fc22f770c5c7803262111e
Vyom Mani Tiwari <vyommani@gmail.com>
2026-06-17 15:59:07 +0530

    RANGER-5619: Keyadmin user unable to perform test connection for cm_kms service

--- security-admin/src/test/java/org/apache/ranger/rest/TestServiceREST.java
+++ security-admin/src/test/java/org/apache/ranger/rest/TestServiceREST.java
@@ -1134,6 +1134,44 @@ public class TestServiceREST {
     }
 
     @Test
+    public void test35eValidateConfig_KeyAdminUser_KmsService_Succeeds() throws Exception {
+        RangerService rangerService = rangerService();
+        rangerService.setType("cm_kms");
+        Mockito.when(bizUtil.isAdmin()).thenReturn(false);
+        Mockito.when(bizUtil.isKeyAdmin()).thenReturn(true);
+        XXServiceDef xServiceDef = serviceDef();
+        XXServiceDefDao xServiceDefDao = Mockito.mock(XXServiceDefDao.class);
+        xServiceDef.setImplclassname(EmbeddedServiceDefsUtil.KMS_IMPL_CLASS_NAME);
+        Mockito.when(daoManager.getXXServiceDef()).thenReturn(xServiceDefDao);
+        Mockito.when(xServiceDefDao.findByName("cm_kms")).thenReturn(xServiceDef);
+        Mockito.when(serviceMgr.validateConfig(rangerService, svcStore)).thenReturn(vXResponse);
+        VXResponse result = serviceREST.validateConfig(rangerService);
+        Assertions.assertNotNull(result);
+        Mockito.verify(bizUtil).isAdmin();
+        Mockito.verify(bizUtil).isKeyAdmin();
+        Mockito.verify(serviceMgr).validateConfig(rangerService, svcStore);
+    }
+
+    @Test
+    public void test35fValidateConfig_KeyAdminUser_NonKmsService_ThrowsForbidden() throws Exception {
+        RangerService rangerService = rangerService();
+        rangerService.setType("hdfs");
+        Mockito.when(bizUtil.isAdmin()).thenReturn(false);
+        Mockito.when(bizUtil.isKeyAdmin()).thenReturn(true);
+        XXServiceDef xServiceDef = serviceDef();
+        XXServiceDefDao xServiceDefDao = Mockito.mock(XXServiceDefDao.class);
+        xServiceDef.setImplclassname("org.apache.ranger.services.hdfs.RangerServiceHdfs");
+        Mockito.when(daoManager.getXXServiceDef()).thenReturn(xServiceDefDao);
+        Mockito.when(xServiceDefDao.findByName("hdfs")).thenReturn(xServiceDef);
+        Mockito.when(restErrorUtil.createRESTException(Mockito.eq(HttpServletResponse.SC_FORBIDDEN), Mockito.anyString(), Mockito.eq(true)))
+                .thenReturn(new WebApplicationException(HttpServletResponse.SC_FORBIDDEN));
+        Assertions.assertThrows(WebApplicationException.class, () -> serviceREST.validateConfig(rangerService));
+        Mockito.verify(bizUtil).isAdmin();
+        Mockito.verify(bizUtil).isKeyAdmin();
+        Mockito.verify(serviceMgr, Mockito.never()).validateConfig(Mockito.any(), Mockito.any());
+    }
+
+    @Test
     public void test40applyPolicy() {
         RangerPolicy existingPolicy = rangerPolicy();
         RangerPolicy appliedPolicy  = rangerPolicy();
