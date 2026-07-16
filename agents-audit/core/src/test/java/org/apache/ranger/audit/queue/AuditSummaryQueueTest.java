cf0869bde0e71f0e5a8cfdbb64673736db3e7762
ChinmayHegde24 <hegdechinmay24@gmail.com>
2026-07-16 16:48:25 +0530

    RANGER-5378: Replaced wrong import with correct one

--- agents-audit/core/src/test/java/org/apache/ranger/audit/queue/AuditSummaryQueueTest.java
+++ agents-audit/core/src/test/java/org/apache/ranger/audit/queue/AuditSummaryQueueTest.java
@@ -34,13 +34,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doThrow;
