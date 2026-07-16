7134c629719c826cf96766155fded39793cd83c4
ChinmayHegde24 <hegdechinmay24@gmail.com>
2026-07-16 16:54:41 +0530

    RANGER-5379: Replaced wrong import with correct one

--- agents-audit/core/src/test/java/org/apache/ranger/audit/provider/AsyncAuditProviderTest.java
+++ agents-audit/core/src/test/java/org/apache/ranger/audit/provider/AsyncAuditProviderTest.java
@@ -26,13 +26,13 @@ import org.junit.jupiter.api.Test;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicLong;
 
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 
 /**
