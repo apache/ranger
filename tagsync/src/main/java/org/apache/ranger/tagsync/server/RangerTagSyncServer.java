2dfd70d8ce3708f42931f3b2a904d0313581836a
sanket-shelar <sanket.shelar.dev@gmail.com>
2026-07-23 20:22:49 +0530

    RANGER-4153: Ranger Tagsync - Convert to Web Application

---
+++ tagsync/src/main/java/org/apache/ranger/tagsync/server/RangerTagSyncServer.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.ranger.tagsync.server;
+
+import org.apache.ranger.tagsync.process.TagSyncConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class RangerTagSyncServer {
+    private static final Logger LOG = LoggerFactory.getLogger(RangerTagSyncServer.class);
+
+    private RangerTagSyncServer() {
+        //to stop instantiation
+    }
+
+    public static void main(String[] args) {
+        LOG.info("==>> RangerTagSyncServer.main()");
+
+        TagSyncConfig config = TagSyncConfig.getInstance();
+        try {
+            EmbeddedServer server = new EmbeddedServer(config, "ranger-tagsync", "ranger.tagsync.");
+            server.start();
+        } catch (Throwable e) {
+            LOG.error("Failed to initialize embedded server due to: ", e);
+            System.exit(1);
+        }
+    }
+}
